e93ccdc6837faac83b6fcffe4bf69bf02afb78ca
joao-conde <joaoconde_13@hotmail.com>
2018-03-06 19:02:20 +0000

    d4 p2 done

--- aoc2017/src/day4.py
+++ aoc2017/src/day4.py
@@ -2,14 +2,13 @@
 
 #For current repos config path is '../res/d4input.txt'
 
-#src = input("Input file path + extension (e.g.: /dir/file.txt): ")
-src = '../res/d4input.txt'
+src = input("Input file path + extension (e.g.: /dir/file.txt): ")
 input_file = open(src)
-
+lines = input_file.readlines()
 
 #PART1
 
-def validPassPhrase(pp):
+def validPassPhraseP1(pp):
     words = pp.split(' ')
     
     for word in words:
@@ -18,16 +17,46 @@ def validPassPhrase(pp):
     return True
 
 
-validPPs = 0
+validPPs1 = 0
 
-lines = input_file.readlines()
+#PART2
 
+#checks if 2 words are anagrams of each other ; true if so false if not
+def isAnagram(word1, word2):
+    if len(word1) != len(word2) : return False
+        
+    for letter in word1:
+        if word2.count(letter) == 0 : return False
+
+    return True
+
+def validPassPhraseP2(pp):
+    wordsList = pp.split()
+
+    while len(wordsList) > 1:
+        head = wordsList[0]
+        tail = wordsList[1:]
+
+        for word in tail:
+            if(isAnagram(head, word)) : return False
+        
+        wordsList = tail
+    
+    return True
+
+
+validPPs2 = 0
 
 for line in lines:
     line = line.replace('\n', '')
-    if validPassPhrase(line) : validPPs += 1
+    if validPassPhraseP1(line) : validPPs1 += 1
+    if validPassPhraseP2(line) : validPPs2 += 1
+
+#Results print
+print("\nP1 valid passphrases: " + str(validPPs1))
+print("\nP2 valid passphrases: " + str(validPPs2))
 
 
-print("\nValid passphrases: " + str(validPPs))
+
 
 
