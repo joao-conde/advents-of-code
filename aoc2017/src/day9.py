a905993cb3f9fbb3ec9ff6873717f387ad7e3d80
joao-conde <joaoconde_13@hotmail.com>
2018-03-18 22:15:12 +0000

    day9 clean of ! and escaped chars and <> garbage

--- aoc2017/src/day9.py
+++ aoc2017/src/day9.py
@@ -5,22 +5,45 @@
 src = "../res/d9input.txt"
 #src = input("Input file path + extension (e.g.: /dir/file.txt): ")
 input_file = open(src)
-input_group = input_file.read().split('\n')
+input_group = input_file.read()
 input_file.close()
 
 
 #PART1 
 
+
+#clean group string: remove ! and the char they negate
+i = 0
+while i < len(input_group):
+
+    if input_group[i] == '!' : 
+        input_group = input_group[:i] + input_group[(i+1):]
+        input_group = input_group[:i] + input_group[(i+1):] 
+
+    i += 1
+    
 print(input_group)
 
+#clean the garbage - in between '<...>'
+i = 0
+garbage = False
+while i < len(input_group):
 
+    if input_group[i] == '>' :
+        input_group = input_group[:i] + input_group[(i+1):]
+        garbage = False
+        i -= 1
 
+    if input_group[i] == '<' :
+        garbage = True 
+
+    if garbage : 
+        input_group = input_group[:i] + input_group[(i+1):]
+        i -= 1
+
+    i += 1
     
         
 
 
-    
-    
-
-
-
+print(input_group)
