9d99364007eee36d77642e471b57d5cd6f661c54
joao-conde <joaoconde_13@hotmail.com>
2018-03-19 18:11:33 +0000

    d9 calculate score of a group

--- aoc2017/src/day9.py
+++ aoc2017/src/day9.py
@@ -1,6 +1,24 @@
 #Link to problem: https://adventofcode.com/2017/day/9
 
 
+#counts number of groups inside a group at that level(groups delimited by {...} )
+def count_groups(group):
+    ngroups = 0
+    group_open = False
+    
+    for char in group:
+        if char == '{': group_open = True
+        if char == '}' and group_open : 
+            group_open = False
+            ngroups += 1
+
+    return ngroups
+
+#calculates the score of a group level ---> level score = group level * number of groups
+def calculate_score(lvl, group):
+    return lvl * count_groups(group)
+
+
 #For current repos config path is '../res/d9input.txt'
 src = "../res/d9input.txt"
 #src = input("Input file path + extension (e.g.: /dir/file.txt): ")
@@ -10,7 +28,6 @@ input_file.close()
 
 
 #PART1 
-
 
 #clean group string: remove ! and the char they negate
 i = 0
@@ -45,5 +62,5 @@ while i < len(input_group):
     
         
 
-
 print(input_group)
+print(calculate_score(2,input_group))
