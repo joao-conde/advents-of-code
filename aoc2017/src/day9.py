345860300d31984d8cd2269631773672db43bb0c
joao-conde <joaoconde_13@hotmail.com>
2018-03-21 18:44:37 +0000

    ...

--- aoc2017/src/day9.py
+++ aoc2017/src/day9.py
@@ -1,22 +1,24 @@
 #Link to problem: https://adventofcode.com/2017/day/9
 
 
-#counts number of groups inside a group at that level(groups delimited by {...} )
-def count_groups(group):
-    ngroups = 0
-    group_open = False
-    
-    for char in group:
-        if char == '{': group_open = True
-        if char == '}' and group_open : 
-            group_open = False
-            ngroups += 1
+#get subgroups
+def get_subgroups(group):
+    subgroups = []
 
-    return ngroups
+    i = 1
+    while i < len(group): 
+        
+        if group[i] == '{' and group[i+1] != '{':
+            j = i
+            while j < len(group):
+                if group[j] == '}' :
+                    print(group[i:j+1])
+                    break
+                j += 1
 
-#calculates the score of a group level ---> level score = group level * number of groups
-def calculate_score(lvl, group):
-    return lvl * count_groups(group)
+        i += 1
+
+    return subgroups
 
 
 #For current repos config path is '../res/d9input.txt'
@@ -25,6 +27,13 @@ src = "../res/d9input.txt"
 input_file = open(src)
 input_group = input_file.read()
 input_file.close()
+print(input_group)
+
+
+subgroups = get_subgroups(input_group)
+for subgroup in subgroups:
+    print(subgroup)
+    subgroups.append(get_subgroups(subgroup))
 
 
 #PART1 
@@ -39,7 +48,6 @@ while i < len(input_group):
 
     i += 1
     
-print(input_group)
 
 #clean the garbage - in between '<...>'
 i = 0
@@ -60,7 +68,3 @@ while i < len(input_group):
 
     i += 1
     
-        
-
-print(input_group)
-print(calculate_score(2,input_group))
