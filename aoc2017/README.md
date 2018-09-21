14195af1658341f407af483f8fe49e5337c9e2b3
João Conde <up201503256@fe.up.pt>
2018-09-21 16:13:09 +0100

    day12 p2 done, readme updated

--- aoc2017/README.md
+++ aoc2017/README.md
@@ -2,4 +2,3 @@ Incomplete exercises in src:
 
 1. day9
 2. day10
-3. day12
--- aoc2017/src/day12.py
+++ aoc2017/src/day12.py
@@ -1,8 +1,10 @@
 #Link to problem: https://adventofcode.com/2017/day/12
      
 
-def find_connections(prog, connections, explored):
+def find_connections(prog, connections, explored = None):
     
+    if explored == None: explored = []
+
     programs = set()
 
     for program in connections[prog]:
@@ -24,15 +26,15 @@ def find_connections(prog, connections, explored):
 
 
 #For current repos config path is '../res/d12input.txt'
-#src = input("Input file path + extension (e.g.: /dir/file.txt): ")
 
-src = '../res/d12input.txt'
+src = input("Input file path + extension (e.g.: /dir/file.txt): ")
+
 input_file = open(src)
 pipes = input_file.read().split('\n')
 input_file.close()
 
 
-#PART 1
+#PART 1 & 2
 
 connections = []
 
@@ -46,10 +48,17 @@ for pipe in pipes:
     #List of connections as program sets
     connections.append(set(pipe))
 
+
 connected_to_0 = 0
+groups = set()
+
 for i in range(len(connections)):
-    group = find_connections(i, connections, [])
+    
+    group = frozenset(find_connections(i, connections)) #frozenset indicates python we do not want to mutate the inner sets
+    groups.add(group)
+
     if 0 in group: connected_to_0 += 1
 
 
-print("Programs in PROGRAM 0 group:", connected_to_0)
+print("\nPrograms in PROGRAM 0 group:", connected_to_0)
+print("Number of groups:", len(groups))
