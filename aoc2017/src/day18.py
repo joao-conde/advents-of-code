48b8b0694a735c7835c81a09a7868e6f98a05dbe
João Conde <up201503256@fe.up.pt>
2018-09-30 21:31:08 +0100

    aoc17 d18

---
+++ aoc2017/src/day18.py
@@ -0,0 +1,14 @@
+#Link to problem: https://adventofcode.com/2017/day/18
+
+
+#For current repos config path is '../res/d18.txt'
+#src = input("Input file path + extension (e.g.: /dir/file.txt): ")
+
+src = "../res/d18.txt"
+
+input_file = open(src)
+instructions = input_file.read().split('\n')
+input_file.close()
+
+for i in range(len(instructions)): 
+    print("big pile of shit input and problem explanation :!D;!D=")
