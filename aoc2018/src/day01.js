fff9be606691b5fe553dd48c0d4fb939d49723e3
João Conde <up201503256@fe.up.pt>
2018-12-02 17:40:07 +0000

    Day1 p1&p2

---
+++ aoc2018/src/day01.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const input = fs.readFileSync('../res/d01').toString().split('\n');
+
+solveP1 = () => {
+	console.log('Resulting frequency is ' + input.map(Number).reduce((acc, currVal) => acc + currVal));
+};
+
+solveP2 = () => {
+	const previousFreqVals = new Set();
+
+	let frequency = 0, i = 0;
+	while(!previousFreqVals.has(frequency)){
+		previousFreqVals.add(frequency);
+		
+		frequency += parseInt(input[i]);
+
+		if(i + 1 == input.length)
+			i = 0;
+		else
+			i++;
+	}
+
+	console.log('First repeated frequency is ' + frequency);
+};
+
+solveP1();
+solveP2();
