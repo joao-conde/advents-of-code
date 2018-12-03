87ddec7f32924a611a9f9c2aa261d56faa068ec2
João Conde <up201503256@fe.up.pt>
2018-12-03 14:18:10 +0000

    Refactoring

--- aoc2018/src/day01.js
+++ aoc2018/src/day01.js
@@ -1,28 +1,15 @@
 //Link to problem: https://adventofcode.com/2018/day/1
-const fs = require('fs');
-const input = fs.readFileSync('../res/d01').toString().split('\n');
+const input = require('fs').readFileSync('../res/d01').toString().split('\n');
 
-solveP1 = () => {
-	console.log('Resulting frequency is ' + input.map(Number).reduce((acc, currVal) => acc + currVal));
-};
-
-solveP2 = () => {
-	const previousFreqVals = new Set();
-	let frequency = 0, i = 0;
+const previousFreqVals = new Set();
+let frequency = 0, i = 0;
+while(!previousFreqVals.has(frequency)){
+	previousFreqVals.add(frequency);
+	frequency += parseInt(input[i]);
 	
-	while(!previousFreqVals.has(frequency)){
-		previousFreqVals.add(frequency);
-		
-		frequency += parseInt(input[i]);
+	if(i + 1 == input.length) i = 0;
+	else i++;
+}
 
-		if(i + 1 == input.length)
-			i = 0;
-		else
-			i++;
-	}
-
-	console.log('First repeated frequency is ' + frequency);
-};
-
-solveP1();
-solveP2();
+console.log('Resulting frequency is ' + input.map(Number).reduce((acc, currVal) => acc + currVal));
+console.log('First repeated frequency is ' + frequency);
--- aoc2018/src/day02.js
+++ aoc2018/src/day02.js
@@ -1,42 +1,33 @@
 //Link to problem: https://adventofcode.com/2018/day/2
-const fs = require('fs');
-const input = fs.readFileSync('../res/d02').toString().split('\n');
+const input = require('fs').readFileSync('../res/d02').toString().split('\n');
 
-//P1
 buildOccurMap = (word) => {
 	let map = {};
 	word.split('').forEach(letter => letter in map ? map[letter]++ : map[letter] = 1);
 	return map;
 };
 
-containsNOccur = (n, occurrencyMap) => { 
-	for(const key in occurrencyMap){
-		if(occurrencyMap[key] == n) return true;
+containsNOccur = (n, occurrencyMap) => {
+	for (const key in occurrencyMap) {
+		if (occurrencyMap[key] == n) return true;
 	}
 	return false;
 }
 
-solveP1 = () => {
-	console.log('Checksum is ' + input.filter(word => {return containsNOccur(2, buildOccurMap(word))}).length
-				* input.filter(word => {return containsNOccur(3, buildOccurMap(word))}).length);
-};
-
-//P2
 getCommonSubstring = (str1, str2) => {
-	const zip = str1.split('').map((e, i) => {return [e, str2.split('')[i]]});
-	return zip.filter(e => {return e[0] == e[1]}).map(e => e[0]).join('');
+	const zip = str1.split('').map((e, i) => { return [e, str2.split('')[i]] });
+	return zip.filter(e => { return e[0] == e[1] }).map(e => e[0]).join('');
 };
 
-solveP2 = () => {
-	let largestCommonStr = '';
-	input.forEach(str1 => input.forEach(str2 => {
-		if(str1 != str2){
-			const common = getCommonSubstring(str1, str2);
-			if(largestCommonStr.length < common.length) largestCommonStr = common;
-		}
-	}));
-	console.log('Common letters between correct box ID\'s are ' + largestCommonStr);
-};
+let largestCommonStr = '';
+input.forEach(str1 => input.forEach(str2 => {
+	if (str1 != str2) {
+		const common = getCommonSubstring(str1, str2);
+		if (largestCommonStr.length < common.length) largestCommonStr = common;
+	}
+}));
 
-solveP1();
-solveP2();
+console.log('Checksum is ' + input.filter(word => { return containsNOccur(2, buildOccurMap(word)) }).length
+			* input.filter(word => { return containsNOccur(3, buildOccurMap(word)) }).length);
+
+console.log('Common letters between correct box ID\'s are ' + largestCommonStr);
