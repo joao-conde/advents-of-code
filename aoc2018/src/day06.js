85c25d1f1a219d2a2c1c28fd1357db89186f8b31
João Conde <up201503256@fe.up.pt>
2018-12-08 02:38:03 +0000

    Day6 p2 + comments

--- aoc2018/src/day06.js
+++ aoc2018/src/day06.js
@@ -1,12 +1,18 @@
-// Link to problem: https://adventofcode.com/2018/day/6
+/* Link to problem: https://adventofcode.com/2018/day/6 */
 const input = require('fs').readFileSync('../res/d06').toString().split('\n');
 
+/* Regions count if above the threshold */
+const SAFE_THRESHOLD = 10000;
+
+/* Calculates Manhattan Distance */
 const computeManhattanDistance = (x0, y0, x1, y1) => {
 	return Math.abs(x0 - x1) + Math.abs(y0 - y1);
 };
 
+let safeSpots = 0; /* Spots above the threshold for P2 */
+const infinite = new Set(); /* Set of points that extend to infinity */
 const coords = input.map((el) => [parseInt(el.split(',')[0]) - 1, parseInt(el.split(',')[1]) - 1]);
-const infinite = new Set();
+const areas = Array(coords.length).fill(0); /* Count of cells for each point area */
 const grid = Array(Math.max(...coords.map((el) => el[0]))+1).fill()
 	.map(() => Array(Math.max(...coords.map((el) => el[1]))+1).fill('.'));
 
@@ -15,18 +21,23 @@ grid.forEach((row, r, grid) => {
 		const distances = coords.map((coord) => {
 			return computeManhattanDistance(r, e, coord[0], coord[1]);
 		});
+
+		if (distances.reduce((acc, cur) => acc + cur) < SAFE_THRESHOLD) safeSpots++;
+
 		const minimalDistance = Math.min(...distances);
 		const unique = distances.filter((dis) => dis == minimalDistance).length;
 		grid[r][e] = (unique == 1 ? distances.indexOf(minimalDistance) : '.');
+
+		/* If its an outside of the matrix that point extends to infinity */
 		if (r == 0 || r == grid.length-1 || e == 0 || e == row.length-1) infinite.add(grid[r][e]);
 	});
 });
 
-const areas = Array(coords.length).fill(0);
 grid.forEach((row) => {
 	row.forEach((el) => {
 		if (el >= 0 && el < areas.length && !infinite.has(el)) areas[el]++;
 	});
 });
 
-console.log('P1 - The size of the largest finite area is ' + Math.max(...areas));
+console.log('P1 - The size for dangerous coordinates of the largest finite area is ' + Math.max(...areas));
+console.log('P2 - The size for safe coordinates of the largest finite area is ' + safeSpots);
