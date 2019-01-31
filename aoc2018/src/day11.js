8aef5e7a6159cb10a075d032f9f303fca679f9ab
joao-conde <joaoconde_13@hotmail.com>
2019-01-31 00:13:50 +0000

    day11 start

---
+++ aoc2018/src/day11.js
@@ -0,0 +1,23 @@
+/* Link to problem: https://adventofcode.com/2018/day/11 */
+const input = parseInt(require('fs').readFileSync('../res/d11').toString());
+
+const computePowerLvl = (x, y, serial) => {
+    const rackID = x + 10;
+    const powerLvl = ((rackID * y) + serial) * rackID; 
+    return ((powerLvl % 1000 - powerLvl % 100) / 100) - 5;
+};
+
+
+
+
+
+
+
+
+
+
+// tests
+// console.log(computePowerLvl(3, 5, 8));
+// console.log(computePowerLvl(122, 79, 57));
+// console.log(computePowerLvl(217, 196, 39));
+// console.log(computePowerLvl(101, 153, 71));
