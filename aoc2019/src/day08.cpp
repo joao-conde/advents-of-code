ce5f78eb385a6c39df0569c3d15254139ce00c15
Joao Conde <joaodiasconde@gmail.com>
2019-12-09 15:50:35 +0100

    Day8 p2

--- aoc2019/src/day08.cpp
+++ aoc2019/src/day08.cpp
@@ -7,11 +7,17 @@
 
 using namespace std;
 
-
 int getLayerValCnt(const vector<vector<int>> &layer, int val){
     vector<int> rowCnts;
     transform(layer.begin(), layer.end(), back_inserter(rowCnts), [&val](vector<int> row){return count(row.begin(), row.end(), val);});
     return accumulate(rowCnts.begin(), rowCnts.end(), 0, [](int acc, int x){return acc + x;});
+}
+
+int computePixelColor(const vector<vector<vector<int>>> &image, int w, int h){
+    for(int i = 0; i < image.size(); i++){
+        if(image[i][w][h] != 2) return image[i][w][h];
+    }
+    return 0;
 }
 
 int main(){
@@ -20,9 +26,9 @@ int main(){
     vector<vector<vector<int>>> image;
     while(input.peek() != EOF){
         vector<vector<int>> layer;
-        for(int i = 0; i < height; i++){
+        for(int i = 0; i < width; i++){
             vector<int> row;
-            for(int j = 0; j < width; j++){
+            for(int j = 0; j < height; j++){
                 char pixel;
                 input >> pixel;
                 row.push_back(stoi(&pixel));
@@ -38,4 +44,11 @@ int main(){
     transform(image.begin(), image.end(), back_inserter(zeroCnt), [](vector<vector<int>> layer){return getLayerValCnt(layer, 0);});
     int fewestZeroLayerIdx = distance(zeroCnt.begin(), min_element(zeroCnt.begin(), zeroCnt.end()));
     cout << "Part1: " << getLayerValCnt(image[fewestZeroLayerIdx], 1) * getLayerValCnt(image[fewestZeroLayerIdx], 2) << endl;
+
+    cout << "Part2" << endl;
+    for(int i = 0; i < width; i++) {
+        for(int j = 0; j < height; j++) 
+            cout << computePixelColor(image, i, j) << " ";
+        cout << endl;
+    }
 }
