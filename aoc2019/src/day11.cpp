a1b62b047d6b6c215dc82a0145429ead03b7b435
Joao Conde <joaodiasconde@gmail.com>
2019-12-11 23:49:33 +0100

    Day11 p2 - i hate printing 2d matrixes to read results :)

--- aoc2019/src/day11.cpp
+++ aoc2019/src/day11.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <queue>
@@ -107,23 +108,14 @@ struct PointHash{
     }
 };
 
-int main(){
-    vector<long long int> intcode;
-    ifstream input("../res/day11");
-    while(!input.eof()){
-        long long int code;
-        input >> code;
-        input.ignore();
-        intcode.push_back(code);
-    }
-    input.close();
-
+unordered_map<pair<int,int>, char, PointHash> buildPointColorMap(vector<long long int> intcode, char startColor = '.'){
     IntcodeProgram program(intcode, intcode.size());
     unordered_map<pair<int,int>, char, PointHash> pointColorMap; //. -> black, # -> white
     char dirs[] = {'^', '>', 'v', '<'};
     
     int curDir = 0;
     pair<int,int> curPoint = make_pair(0, 0);
+    pointColorMap[curPoint] = startColor;
     while(!program.halt){
         auto pointIt = pointColorMap.find(curPoint);
         char curColor = (pointIt != pointColorMap.end() ? pointIt->second : '.');
@@ -149,5 +141,32 @@ int main(){
         }
     }
 
-    cout << "Part1: The number of at least once painted panels is " << pointColorMap.size() << endl;
+    return pointColorMap;
+}
+
+int main(){
+    vector<long long int> intcode;
+    ifstream input("../res/day11");
+    while(!input.eof()){
+        long long int code;
+        input >> code;
+        input.ignore();
+        intcode.push_back(code);
+    }
+    input.close();
+
+    int areaEstimate = buildPointColorMap(intcode).size();
+    cout << "Part1: The number of at least once painted panels is " << areaEstimate << endl;
+    
+    unordered_map<pair<int,int>, char, PointHash> hullID = buildPointColorMap(intcode, '#');
+    int sideEstimate = sqrt(areaEstimate);
+    cout << "Part2" << endl;
+    for(int i = -sideEstimate; i < sideEstimate; i++){
+        for(int j = -sideEstimate; j < sideEstimate; j++){
+            pair<int,int> point = make_pair(i, j);
+            auto pointIt = hullID.find(point);
+            cout << (pointIt != hullID.end() ? pointIt->second : '.');
+        }
+        cout << endl;
+    }
 }
