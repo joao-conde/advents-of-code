b1e2ee743c5dde513f031b814230a061192d4529
Joao Conde <joaodiasconde@gmail.com>
2019-12-15 18:56:53 +0100

    Day10 p2

--- aoc2019/src/day10.cpp
+++ aoc2019/src/day10.cpp
@@ -4,32 +4,34 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
-int countVisibleAsteroids(const vector<string> &map, int x, int y){
-    unordered_set<double> lineOfSightAngles; //angles of lines of sight vectors from (x, y) that intersect another asteroid
+typedef unordered_map<double, pair<int,int>> doubleIntPairMap;
+
+double angleBetweenVectors(int x1, int y1, int x2, int y2){
+    pair<double, double> vector = make_pair(x1 - x2, y1 - y2);
+    double vectorNorm = sqrt(vector.first * vector.first + vector.second * vector.second);            
+    pair<double, double> normalizedVector = make_pair(vector.first / vectorNorm, vector.second / vectorNorm);
+
+    // cos(alpha) = a·b / |a|·|b| ----> b = (0, -1)
+    // becomes y of vec a dividing by 1 (unit vectors) 
+    // alpha = acos(-a.y / 1) = acos(a.y) 
+    double angle = round(acos(-normalizedVector.second) * 1000) / 1000; //round to 3 decimal place
+    return (normalizedVector.first < 0 ? 2 * M_PI - angle : angle);
+}
+
+doubleIntPairMap getVisibleAsteroids(const vector<string> &map, int x, int y){
+    doubleIntPairMap asteroidAngles; //angles of lines of sight vectors from (x, y) that intersect another asteroid
     for(int i = 0; i < map.size(); i++){
         for(int j = 0; j < map[i].size(); j++){
-            if(i == x && j == y) continue; //self
+            if(i == y && j == x) continue; //self
             if(map[i][j] == '.') continue; //no asteroid
-
-            pair<double, double> vector = make_pair(i - x, j - y);
-            double vectorNorm = sqrt(vector.first * vector.first + vector.second * vector.second);            
-            pair<double, double> normalizedVector = make_pair(vector.first / vectorNorm, vector.second / vectorNorm);
-
-            // cos(alpha) = a·b / |a|·|b| ----> b = (0,1)
-            // becomes y of vec a dividing by 1 (unit vectors)
-            // alpha = acos(a.y / 1) = acos(a.y) 
-            double angle = round(acos(normalizedVector.second) * 1000) / 1000; //round to 3 decimal place
-
-            if (normalizedVector.first < 0) angle = 2* M_PI -angle;
-
-            lineOfSightAngles.insert(angle);
+            asteroidAngles[angleBetweenVectors(j, i, x, y)] = make_pair(j, i);
         }
     }
-    return lineOfSightAngles.size();
+    return asteroidAngles;
 }
 
 int main(){
@@ -38,10 +40,35 @@ int main(){
     string line;
     while(getline(input, line)) map.push_back(line);
 
-    vector<int> visibleAsteroids;
+    int maxAsteroids = 0;
+    pair<int,int> station;
     for(int i = 0; i < map.size(); i++) 
         for(int j = 0; j < map.size(); j++)
-            if(map[i][j] != '.') visibleAsteroids.push_back(countVisibleAsteroids(map, i, j));
+            if(map[i][j] != '.'){
+                doubleIntPairMap asteroidAngles = getVisibleAsteroids(map, j, i);
+                if(asteroidAngles.size() > maxAsteroids){
+                    maxAsteroids = asteroidAngles.size();
+                    station = make_pair(j, i);
+                }
+            }
 
-    cout << "Part1: Maximum visible asteroids are " << *max_element(visibleAsteroids.begin(), visibleAsteroids.end()) << endl;
+    cout << "Part1: Maximum visible asteroids are " << maxAsteroids << endl;
+
+
+    int destroyed = 0, ax, ay;
+    while(destroyed < 200){
+        doubleIntPairMap asteroidAngles = getVisibleAsteroids(map, station.first, station.second);
+        vector<double> angles;
+        transform(asteroidAngles.begin(), asteroidAngles.end(), back_inserter(angles), [](auto kv){return kv.first;});
+        sort(angles.begin(), angles.end());
+        for(int i = 0; i < angles.size(); i++){
+            ax = asteroidAngles[angles[i]].first;
+            ay = asteroidAngles[angles[i]].second;
+            map[ax][ay] = '.';
+            destroyed++;
+            if(destroyed == 200) break;
+        }
+    }
+
+    cout << "Part2: 200th asteroid destroyed was at (" << ax << ", " << ay << ") = " << ax * 100 + ay << endl;
 }
