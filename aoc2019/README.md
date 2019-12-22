783962ebe03e14e6057d1e92f9596a49b65c458f
Joao Conde <joaodiasconde@gmail.com>
2019-12-22 00:40:52 +0000

    Day14 p1 with a bug

---
+++ aoc2019/README.md
@@ -0,0 +1,27 @@
+# [Advent of Code 2019](https://adventofcode.com/2019)
+
+## Why C++
+
+Felt like practicing my first programming language. 
+
+Widely used for systems programming, C++ is one of those languages that forces you to think on many low level aspects and define perfectly well your data structures.
+
+Next year may take the time to learn Go or Rust as these are gradually overtaking C++ in systems programming.
+
+
+## Run solutions
+
+Compiled using g++ version:
+```
+g++ (GCC) 9.2.0
+```
+
+Using:
+```
+g++ day<DAY_NUMBER>.cpp -o exe
+```
+
+And running with
+```
+./exe
+```
--- aoc2019/src/day14.cpp
+++ aoc2019/src/day14.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -7,17 +8,41 @@
 
 using namespace std;
 
-struct MapHash{ 
-    size_t operator()(const pair<string,int> &k) const{
-        return hash<string>{}(k.first + to_string(k.second));
-    }
-};
+typedef unordered_map<string, pair<unordered_map<string, int>, int>> reactionsList;
 
-typedef unordered_map<pair<string, int>, vector<pair<string, int>>, MapHash> reactionsList;
-
-unordered_map<string, int> findBasicComponents(string component, const reactionsList &list, unordered_map<string, int> &components){
-
+bool isBasicComponent(string component, const reactionsList &reactions){
+    unordered_map<string, int> inputs = reactions.find(component)->second.first;
+    auto oreIT = inputs.find("ORE");
+    return (oreIT != inputs.end() && inputs.size() == 1);
 }
+
+void findBasicComponentsQuantity(string component, int mult, unordered_map<string, int> &list, const reactionsList &reactions){
+    unordered_map<string, int> inputs = reactions.find(component)->second.first;
+    
+    for(auto kv: inputs){
+        if(isBasicComponent(kv.first, reactions))
+            list[kv.first] += kv.second * mult;
+        else
+            findBasicComponentsQuantity(kv.first, kv.second * mult, list, reactions);
+    }
+}
+
+int computeMinimumORE(const unordered_map<string, int> &inputs, const reactionsList &reactions){
+    int ore = 0;
+
+    for(auto kv: inputs){
+        pair<unordered_map<string, int>, int> basicInputs = reactions.find(kv.first)->second;
+        int reactCnt = ceil((double)kv.second / basicInputs.second);
+
+        int tmp = reactCnt * basicInputs.first.find("ORE")->second;
+        ore += tmp;
+
+        // cout << kv.first << endl;
+        // cout << "Produced - ORE: " << basicInputs.second * reactCnt << " - " << tmp << endl;
+    }
+
+    return ore;
+} 
 
 int main(){
     ifstream file("../res/day14");
@@ -30,22 +55,33 @@ int main(){
 
         int quantity;
         string entry, component;
-        vector<pair<string, int>> recipeVec;
+        unordered_map<string, int> recipeQuantities;
         stringstream ss_recipe(recipe);
         while(ss_recipe.good()){
             getline(ss_recipe, entry, ',');
             stringstream ss_entry(entry);
             ss_entry >> quantity >> component;
-            recipeVec.push_back(make_pair(component, quantity));
+            recipeQuantities.insert(make_pair(component, quantity));
         }
 
         stringstream ss_result(result);
         ss_result >> quantity >> component;
-        pair<string, int> resultPair = make_pair(component, quantity);
-        reactions.insert(make_pair(resultPair, recipeVec));
+        reactions.insert(make_pair(component, make_pair(recipeQuantities, quantity)));
     }
 
+    // for(auto kv: reactions){
+    //     cout << kv.second.second << " units of component " << kv.first << " can be done with:" << endl;
+    //     for(auto r: kv.second.first){
+    //         cout << "- " << r.second << " x " << r.first << endl;
+    //     }
+    // }
+
     unordered_map<string, int> fuelBasicComponents;
-    pair<string, int> component = make_pair("FUEL", 1);
-    findBasicComponents("FUEL", reactions, fuelBasicComponents);
+    findBasicComponentsQuantity("FUEL", 1, fuelBasicComponents, reactions);
+
+    // for(auto kv: fuelBasicComponents){
+    //     cout << "Need " << kv.second << " x " << kv.first << endl;
+    // }
+
+    cout << computeMinimumORE(fuelBasicComponents, reactions) << endl;
 }
