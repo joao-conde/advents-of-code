7ebf9c7d5217e29a35ef3be131ea929b162ca601
unknown <joaodiasconde@gmail.com>
2020-05-02 04:04:25 +0100

    d13 p1 2017 frequency heuristic, modulo arithmetic

--- aoc2017/src/day13.py
+++ aoc2017/src/day13.py
@@ -1,88 +1,29 @@
 #Link to problem: https://adventofcode.com/2017/day/13
 
-
-class Layer:
-
-    def __init__(self, range):
-        self.range = range
-        self.reset_scanner()
-
-    def reset_scanner(self):
-        self.scan_area = ['E'] * self.range
-        if self.range > 0: self.scan_area[0] = 'S' #scanner initial position
-        self.scan_dir = 1 #default, 1 to go "down", -1 to go "up"
-
-    def top(self):
-
-        if len(self.scan_area) == 0:
-            return 'E'
-        else:
-            return self.scan_area[0]
-
-    def advance_scanner(self):
-        
-        if self.range > 0:
-            scanner_idx = self.scan_area.index('S')
-            self.scan_area[scanner_idx] = 'E'
-            scanner_idx += self.scan_dir
-            self.scan_area[scanner_idx] = 'S'
-
-            if scanner_idx == self.range-1: self.scan_dir = -1
-            if scanner_idx == 0: self.scan_dir = 1
-
-
-
-    def __str__(self):
-        info = "LAYER RANGE: " + str(self.range) + "\n"
-
-        for el in self.scan_area:
-            info += el + " "
-
-
-        return info
-
-
-#For current repos config path is '../res/d13.txt'
-#src = input("Input file path + extension (e.g.: /dir/file.txt): ")
-
-
-src = '../res/d13.txt'
-
+src = '../res/d13'
 
 input_file = open(src)
-layers = input_file.read()
-layers = [layer.replace(':', '').split() for layer in layers.split('\n')]
+layers = input_file.read().split("\n")
 input_file.close()
 
-#PART 1
-
-#Build firewall
-firewall = []
-depth = 0
-while len(layers) > 0:
-
-    if depth == int(layers[0][0]):
-        layer = Layer(int(layers[0][1]))
-        layers.remove(layers[0])
-    else:
-        layer = Layer(0)
-
-    firewall.append(layer)
-    depth += 1
+layers = [layer.replace(':', '').split() for layer in layers]
+layers_dict = {}
+for pair in layers:
+    layers_dict[int(pair[0])] = int(pair[1])
 
 
-#Compute severity of getting caught
-severity = 0
-picoseconds = len(firewall)
-for ps in range(picoseconds):
+# PART 1
+packet_pos, severity = 0, 0
+max_ps = max([x for x in layers_dict])
+for ps in range(max_ps + 1): 
+    if ps in layers_dict:
+        if ps % (2*(layers_dict[ps] - 1)) == 0: # scanner is at position 0 at frequency 2 * (range - 1)
+            severity += (ps * layers_dict[ps])
+    
+    packet_pos += 1
 
-    if firewall[ps].top() == 'S':
-        severity += ps * firewall[ps].range #depth * range
-
-    for layer in firewall: layer.advance_scanner()
+print("(Part1) Severity of the whole trip:", severity)
 
 
-print("\nSeverity of the whole trip:", severity)
+# PART 2
 
-
-#PART 2
