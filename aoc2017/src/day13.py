9fbdc29b014fc1f1536c9efe66ddf507d8ca2e29
unknown <joaodiasconde@gmail.com>
2020-05-02 04:22:05 +0100

    p2 day13 2017

--- aoc2017/src/day13.py
+++ aoc2017/src/day13.py
@@ -11,19 +11,16 @@ layers_dict = {}
 for pair in layers:
     layers_dict[int(pair[0])] = int(pair[1])
 
-
 # PART 1
-packet_pos, severity = 0, 0
-max_ps = max([x for x in layers_dict])
+severity, max_ps = 0, max([x for x in layers_dict])
 for ps in range(max_ps + 1): 
     if ps in layers_dict:
         if ps % (2*(layers_dict[ps] - 1)) == 0: # scanner is at position 0 at frequency 2 * (range - 1)
             severity += (ps * layers_dict[ps])
-    
-    packet_pos += 1
 
 print("(Part1) Severity of the whole trip:", severity)
 
-
 # PART 2
-
+min_delay = 0
+while 0 in [(ps + min_delay) % (2 * (layers_dict[ps] - 1)) for ps in range(max_ps + 1) if ps in layers_dict]: min_delay += 1
+print("(Part2) Fewest packet delay to pass through the firewall:", min_delay)
