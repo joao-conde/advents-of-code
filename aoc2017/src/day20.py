25868cfd24e8622faee4312fee0c1f4e4332531e
unknown <joaodiasconde@gmail.com>
2020-05-03 20:54:28 +0100

    p1 d20 2017

---
+++ aoc2017/src/day20.py
@@ -0,0 +1,30 @@
+#Link to problem: https://adventofcode.com/2017/day/20
+
+import re
+
+def get_particle_properties(particle_id, particle_info):
+    pattern = "p=<(.*),(.*),(.*)>, v=<(.*),(.*),(.*)>, a=<(.*),(.*),(.*)>"
+    [px, py, pz, vx, vy, vz, ax, ay, az] = [int(x) for x in re.findall(pattern, particle_info)[0]]
+    return {
+        "pid": particle_id,
+        "px": px,
+        "py": py,
+        "pz": pz,
+        "vx": vx, 
+        "vy": vy,
+        "vz": vz, 
+        "ax": ax,
+        "ay": ay, 
+        "az": az
+    }
+
+src = "../res/d20"
+input_file = open(src)
+particle_input = input_file.read().split('\n')
+input_file.close()
+
+# PART 1
+particles_props = [get_particle_properties(pid, particle_info) for (pid, particle_info) in enumerate(particle_input)]
+particles_abs_accel = [(abs(info["ax"]) + abs(info["ay"]) + abs(info["az"]), info["pid"]) for info in particles_props]
+particles_abs_accel.sort() # sorts by acceleration magnitude
+print(f'Closest particle to <0,0,0> in the long term: {particles_abs_accel[0][1]}')
