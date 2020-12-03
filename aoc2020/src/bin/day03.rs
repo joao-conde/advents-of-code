2033c218740d89fada6ddfdb4b6d7f262a29eae6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-03 17:29:09 +0000

    cleanup

--- aoc2020/src/bin/day03.rs
+++ aoc2020/src/bin/day03.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
 const INPUT_PATH: &str = "input/day03";
-const TREE_CHAR: char = '#';
 
 fn main() {
     let input = fs::read_to_string(INPUT_PATH).expect("failure opening input file");
@@ -11,19 +10,16 @@ fn main() {
 }
 
 fn part2(map: &[Vec<char>]) -> u32 {
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    slopes.iter().map(|slope| trees_hit(&map, *slope)).product()
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)].iter().map(|slope| trees_hit(&map, *slope)).product()
 }
 
 fn trees_hit(map: &[Vec<char>], slope: (usize, usize)) -> u32 {
-    let mut trees_hit = 0;
-    let (mut x, mut y) = (0, 0);
+    let (mut x, mut y, mut trees_hit) = (0, 0, 0);
     let (width, height) = (map[0].len(), map.len());
     while y < height {
-        trees_hit += if map[y][x] == TREE_CHAR { 1 } else { 0 };
-        x += slope.0;
+        trees_hit += if map[y][x] == '#' { 1 } else { 0 };
+        x = (x + slope.0) % width;
         y += slope.1;
-        x %= width;
     }
     trees_hit
 }
