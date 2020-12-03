429676665476ff2804b845ed76cdc8633fd140b6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-03 20:26:50 +0000

    2017 d23 thinking

--- aoc2017/src/day23.py
+++ aoc2017/src/day23.py
@@ -49,3 +49,45 @@ for i in range(8): registers[chr(ord('a')+i)] = 0
 p = CoprocessorProgram(registers, instructions)
 p.run()
 print(f'(Part1) Number of mult instructions: {p.mult_instr_cnt}')
+
+
+# PART 2
+a = 1
+b = 107_900
+c = b + 17000
+h = 0
+# test for each x value from b to c
+# stepping 17
+while True:
+    f = 1
+    d = 2
+
+    # test for each d value (from 2)
+    # up until constant d = b
+    while True:
+        e = 2
+
+        # test for each e value
+        # up until constant e = b
+        while True:
+            if d * e - b: f = 0
+            e += 1
+            if e - b: break 
+
+        d += 1
+        if d - b: break
+
+    if f == 0: h += 1
+
+    if b - c: break
+    else: b += 17
+
+
+b = 107_900
+c = b + 17000
+for i in range(b, c, 17):
+    for j in range(2, b):
+        for k in range(2, c):
+            pass
+
+print(h)
