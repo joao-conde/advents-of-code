dec1ffc0f235a0a7dbb3aaad1c02795aba24f2d6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-04 18:00:58 +0000

    Non capturing groups

--- aoc2020/src/bin/day04.rs
+++ aoc2020/src/bin/day04.rs
@@ -15,13 +15,13 @@ fn p1(passports: &[&str]) -> usize {
 
 fn p2(passports: &[&str]) -> usize {
     let regexes = [
-        "byr:(19[2-9][0-9]|200[0-2])",
-        "iyr:20(1[0-9]|20)",
-        "eyr:20(2[0-9]|30)",
-        "hgt:(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)",
+        "byr:(?:19[2-9][0-9]|200[0-2])",
+        "iyr:20(?:1[0-9]|20)",
+        "eyr:20(?:2[0-9]|30)",
+        "hgt:(?:1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)",
         "hcl:#[0-9|a-f]{6}",
-        "ecl:(amb|blu|brn|gry|grn|hzl|oth)",
-        "pid:[0-9]{9}(\n| |$)",
+        "ecl:(?:amb|blu|brn|gry|grn|hzl|oth)",
+        "pid:[0-9]{9}(?:\n| |$)",
     ]
     .iter()
     .map(|re_str| Regex::new(re_str).expect("invalid regex expression"));
