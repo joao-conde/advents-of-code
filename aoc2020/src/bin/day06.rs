4d9f5e969c92aa87c51a6208011f665f54873fe5
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-06 16:36:12 +0000

    ...

--- aoc2020/src/bin/day06.rs
+++ aoc2020/src/bin/day06.rs
@@ -4,14 +4,16 @@ use std::iter::FromIterator;
 
 fn main() {
     let input = fs::read_to_string("input/day06").expect("failure opening input file");
-    let groups: Vec<Vec<HashSet<char>>> =
-        input.split("\n\n").map(|group| group.split('\n').map(|vote| HashSet::from_iter(vote.chars())).collect()).collect();
+    let groups = input
+        .split("\n\n")
+        .map(|group| group.split('\n').map(|vote| HashSet::from_iter(vote.chars())).collect())
+        .collect::<Vec<Vec<HashSet<char>>>>();
 
-    let p1: usize = groups.iter().map(|group| union_sets(group)).map(|s| s.len()).sum();
-    println!("Part1: {:?}", p1);
+    let p1 = groups.iter().map(|group| union_sets(group)).map(|s| s.len()).sum::<usize>();
+    println!("Part1: {}", p1);
 
-    let p2: usize = groups.iter().map(|group| intersect_sets(group)).map(|s| s.len()).sum();
-    println!("Part2: {:?}", p2);
+    let p2 = groups.iter().map(|group| intersect_sets(group)).map(|s| s.len()).sum::<usize>();
+    println!("Part2: {}", p2);
 }
 
 fn union_sets(sets: &[HashSet<char>]) -> HashSet<char> {
