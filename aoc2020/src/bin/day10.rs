bac72b0175931d60ea2be313c67adca84d74980c
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-10 15:33:20 +0000

    Prettify

--- aoc2020/src/bin/day10.rs
+++ aoc2020/src/bin/day10.rs
@@ -3,10 +3,8 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input/day10").expect("failure opening input file");
-
-    let mut jolts = input.split("\n").flat_map(|line| line.parse()).collect::<Vec<u8>>();
+    let mut jolts = input.split('\n').flat_map(|line| line.parse()).collect::<Vec<usize>>();
     jolts.sort();
-        
     jolts.insert(0, 0);
     jolts.push(jolts[jolts.len() - 1] + 3);
 
@@ -14,22 +12,17 @@ fn main() {
         *map.entry(diff).or_insert(0) += 1;
         map
     });
+    println!("Part1: {}", diffs[&1] * diffs[&3]);
 
-    println!("Part1: {:?}", diffs[&1] * diffs[&3]);
-
-
-    let mut dp = vec![0u64; jolts[jolts.len() - 1] as usize + 1]; //todo try remove
-    dp[0] = 1;
-    
-    for jolt in jolts.iter().skip(1) {
-        let jolt = *jolt;
+    let max = jolts[jolts.len() - 1];
+    let mut dp = vec![0u64; max + 1];
+    for jolt in jolts {
         match jolt {
-            1 => dp[jolt as usize] = dp[0],
-            2 => dp[jolt as usize] = dp[0] + dp[1],
-            _ => dp[jolt as usize] = dp[(jolt-1) as usize] + dp[(jolt-2)  as usize] + dp[(jolt-3)  as usize]
+            0 => dp[jolt] = 1,
+            1 => dp[jolt] += dp[jolt - 1],
+            2 => dp[jolt] += dp[jolt - 1] + dp[jolt - 2],
+            _ => dp[jolt] = dp[jolt - 1] + dp[jolt - 2] + dp[jolt - 3],
         }
     }
-
-    println!("Part2: {}", dp[jolts[jolts.len() - 1] as usize]);
-
+    println!("Part2: {}", dp[max]);
 }
