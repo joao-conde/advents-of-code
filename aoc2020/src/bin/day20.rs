d03e40bebb343a1dc0d800e61c466d2ddcc33a3d
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-22 20:54:21 +0000

    day20 p1

--- aoc2020/src/bin/day20.rs
+++ aoc2020/src/bin/day20.rs
@@ -1,64 +1,100 @@
 use std::{fs, mem::swap};
 
-type Tile = Vec<Vec<char>>;
+#[derive(Clone, Debug)]
+struct Tile {
+    pub id: usize,
+    pub img: Vec<Vec<char>>,
+}
+
+impl PartialEq for Tile {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
 
 fn main() {
     let input = fs::read_to_string("input/day20").expect("failure opening input file");
-
     let tiles = input
         .split("\n\n")
         .map(|tile| {
             let mut lines = tile.lines();
             let id = lines.next().unwrap();
             let id = id[5..id.len() - 1].parse().unwrap();
-            let tile = lines.map(|line| line.chars().collect()).collect::<Tile>();
-            (id, tile)
+            let tile = lines.map(|line| line.chars().collect()).collect();
+            Tile { id: id, img: tile }
         })
-        .collect::<Vec<(usize, Tile)>>();
+        .collect::<Vec<Tile>>();
 
-    let size = f32::sqrt(tiles.len() as f32) as usize;
-    println!("{:?}", size);
+    // let size = f32::sqrt(tiles.len() as f32) as usize;
 
-    let img = vec![vec![-1; size]; size];
+    let tiles = tiles.iter().map(|tile| rearrange(tile)).flatten().collect::<Vec<Tile>>();
 
-    let mut test = vec![vec!['A', 'B', 'C'], vec!['D', 'E', 'F'], vec!['G', 'H', 'I']];
+    let mut corners = tiles.iter().filter(|tile| find_matches(tile, &tiles).len() == 2).map(|tile| tile.id).collect::<Vec<usize>>();
 
-    rotate_90(&mut test);
-
-    for line in test {
-        println!("{:?}", line);
-    }
-
-    // create size x size matrix with -1 ids
-    // search for piece that fits
-    // if find one (possibly rotated/flipped), place it, repeat
-    // if none fits, backtrack once
+    corners.dedup();
+    println!("Part1: {}", corners.iter().product::<usize>());
 }
 
-pub fn rotate_90(tile: &mut Tile) {
+fn rearrange(original: &Tile) -> Vec<Tile> {
+    let mut tile = original.clone();
+    let mut combinations = vec![original.clone()];
+    let mut flipped = tile.clone();
+    flip_y(&mut flipped);
+    combinations.push(flipped);
+    for _ in 0..3 {
+        rotate_90(&mut tile);
+        combinations.push(tile.clone());
+        let mut flipped = tile.clone();
+        flip_y(&mut flipped);
+        combinations.push(flipped);
+    }
+    combinations
+}
+
+fn rotate_90(tile: &mut Tile) {
     tranpose(tile);
     flip_y(tile);
 }
 
-pub fn tranpose(tile: &mut Tile) {
-    let mut tmp = vec![vec!['0'; tile[0].len()]; tile.len()];
-    for i in 0..tile.len() {
-        for j in 0..tile[i].len() {
-            tmp[j][i] = tile[i][j];
+fn tranpose(tile: &mut Tile) {
+    let mut tmp = vec![vec!['0'; tile.img[0].len()]; tile.img.len()];
+    for i in 0..tile.img.len() {
+        for j in 0..tile.img[i].len() {
+            tmp[j][i] = tile.img[i][j];
         }
     }
-    swap(tile, &mut tmp);
+    swap(&mut tile.img, &mut tmp);
 }
 
 fn flip_y(tile: &mut Tile) {
-    let size = tile[0].len();
-    for i in 0..tile.len() {
+    let size = tile.img[0].len();
+    for i in 0..tile.img.len() {
         for j in 0..size / 2 {
-            let tmp = tile[i][j];
-            tile[i][j] = tile[i][size - 1 - j];
-            tile[i][size - 1 - j] = tmp;
+            tile.img[i].swap(j, size - 1 - j);
         }
     }
 }
 
-fn get_borders(tile: &mut Tile) {}
+fn get_borders(tile: &Tile) -> Vec<Vec<char>> {
+    let mut borders = vec![];
+    borders.push(tile.img[0][..].to_vec());
+    borders.push(tile.img[tile.img.len() - 1][..].to_vec());
+    let first_col = (0..tile.img.len()).map(|i| tile.img[i][0]).collect();
+    let last_col = (0..tile.img.len()).map(|i| tile.img[i][tile.img[i].len() - 1]).collect();
+    borders.push(first_col);
+    borders.push(last_col);
+    borders
+}
+
+fn find_matches(tile: &Tile, tiles: &Vec<Tile>) -> Vec<Tile> {
+    let tile_borders = get_borders(tile);
+    let mut matches = tiles
+        .clone()
+        .into_iter()
+        .filter(|t| tile.id != t.id)
+        .filter(|t| get_borders(t).iter().any(|border| tile_borders.contains(border)))
+        .collect::<Vec<Tile>>();
+
+    matches.dedup();
+    matches
+}
