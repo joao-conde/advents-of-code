c3b09c23d3057aa5a56b57ba7a876e5f702e7a1c
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-24 03:07:10 +0000

    Part2 day 23

--- aoc2020/src/bin/day23.rs
+++ aoc2020/src/bin/day23.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let labels = "389125467";
-    let cups = labels.chars().map(|c| c.to_digit(10).unwrap() as usize).collect::<Vec<usize>>();
+    let labels = "963275481";
+
+    let mut cups = labels.chars().map(|c| c.to_digit(10).unwrap() as usize).collect::<Vec<usize>>();
+    println!("Part1: {}", p1(&cups));
+
+    (10..1000000 + 1).for_each(|x| cups.push(x));
+    println!("Part2: {}", p2(&cups));
+}
+
+fn game(cups: &Vec<usize>, moves: usize) -> Vec<usize> {
     let min = *cups.iter().min().unwrap();
     let max = *cups.iter().max().unwrap();
 
@@ -11,7 +19,10 @@ fn main() {
     }
 
     let mut cur_cup = cups[0];
-    for _ in 0..100 {
+    for m in 0..moves {
+        if m % 1000000 == 0 {
+            println!("move {}", m + 1);
+        }
         let p1 = list[cur_cup];
         let p2 = list[p1];
         let p3 = list[p2];
@@ -21,7 +32,7 @@ fn main() {
         if dest_cup < min {
             dest_cup = max;
         }
-        while [p1, p2, p3].contains(&dest_cup) || !cups.contains(&dest_cup) {
+        while [p1, p2, p3].contains(&dest_cup) || dest_cup < min || dest_cup > max {
             dest_cup -= 1;
             if dest_cup < min {
                 dest_cup = max;
@@ -37,11 +48,24 @@ fn main() {
         cur_cup = list[cur_cup];
     }
 
+    list
+}
+
+fn p1(cups: &Vec<usize>) -> String {
+    let list = game(&cups, 100);
     let mut p1 = String::new();
     let mut i = 1;
     while list[i] != 1 {
         p1.push_str(&list[i].to_string());
         i = list[i];
     }
-    println!("Part1: {}", p1);
+    p1
+}
+
+fn p2(cups: &Vec<usize>) -> usize {
+    let list = game(&cups, 10000000);
+    let x = list[1];
+    let y = list[x];
+    println!("{} {}", x, y);
+    x * y
 }
