4adecf0edba8888b162290550c4367a245f5cc8b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-24 02:50:19 +0000

    refactor p1 to be efficient for p2

--- aoc2020/src/bin/day23.rs
+++ aoc2020/src/bin/day23.rs
@@ -1,56 +1,47 @@
-use std::collections::VecDeque;
-
 fn main() {
-    let labels = "963275481";
-
-    let mut cups = labels.chars().map(|c| c.to_digit(10).unwrap()).collect::<VecDeque<u32>>();
+    let labels = "389125467";
+    let cups = labels.chars().map(|c| c.to_digit(10).unwrap() as usize).collect::<Vec<usize>>();
     let min = *cups.iter().min().unwrap();
     let max = *cups.iter().max().unwrap();
+
+    // l[x] -> element to which 'x' is is connected
+    let mut list = vec![0; max + 1];
+    for i in 0..cups.len() {
+        list[cups[i]] = cups[(i + 1) % cups.len()];
+    }
+
+    let mut cur_cup = cups[0];
     for _ in 0..100 {
-        let cup = cups[0];
-        let pickup = cups.drain(1..4).collect::<VecDeque<u32>>();
-        let mut dest_cup = cup - 1;
+        let p1 = list[cur_cup];
+        let p2 = list[p1];
+        let p3 = list[p2];
+        list[cur_cup] = list[p3];
+
+        let mut dest_cup = cur_cup - 1;
         if dest_cup < min {
             dest_cup = max;
         }
-        while !cups.contains(&dest_cup) {
+        while [p1, p2, p3].contains(&dest_cup) || !cups.contains(&dest_cup) {
             dest_cup -= 1;
             if dest_cup < min {
                 dest_cup = max;
             }
         }
 
-        while let Some(front) = cups.pop_front() {
-            if front != dest_cup {
-                cups.push_back(front);
-            } else {
-                cups.push_front(front);
-                break;
-            }
-        }
+        let tmp = list[dest_cup];
+        list[dest_cup] = p1;
+        list[p1] = p2;
+        list[p2] = p3;
+        list[p3] = tmp;
 
-        for el in pickup.iter().rev() {
-            cups.insert(1, *el);
-        }
-
-        let cup_i = cups.iter().position(|x| *x == cup).unwrap();
-        let next = cups[(cup_i + 1) % cups.len()];
-        while let Some(front) = cups.pop_front() {
-            if front != next {
-                cups.push_back(front);
-            } else {
-                cups.push_front(front);
-                break;
-            }
-        }
+        cur_cup = list[cur_cup];
     }
 
-    while let Some(front) = cups.pop_front() {
-        if front == 1 {
-            break;
-        } else {
-            cups.push_back(front);
-        }
+    let mut p1 = String::new();
+    let mut i = 1;
+    while list[i] != 1 {
+        p1.push_str(&list[i].to_string());
+        i = list[i];
     }
-    println!("{}", cups.iter().map(|d| d.to_string()).collect::<String>());
+    println!("Part1: {}", p1);
 }
