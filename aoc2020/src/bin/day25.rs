7612e96bb140aded5983590b0a2c723b53e99109
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-25 22:34:29 +0000

    Simplify 25

--- aoc2020/src/bin/day25.rs
+++ aoc2020/src/bin/day25.rs
@@ -2,8 +2,8 @@ use std::fs;
 
 fn main() {
     let pub_keys = fs::read_to_string("input/day25").expect("failure opening input file");
-    let pub_door = pub_keys.lines().next().unwrap().parse().unwrap();
-    let pub_card = pub_keys.lines().nth(1).unwrap().parse().unwrap();
+    let pub_door = pub_keys.lines().next().unwrap().parse::<usize>().unwrap();
+    let pub_card = pub_keys.lines().nth(1).unwrap().parse::<usize>().unwrap();
 
     let mut x = 1;
     let mut loop_size = 0;
@@ -12,16 +12,7 @@ fn main() {
         loop_size += 1;
     }
 
-    let p1 = transform(loop_size, pub_door);
+    let p1 = (0..loop_size).fold(1, |x, _| x * pub_door % 20201227);
     println!("Part1: {}", p1);
     println!("Part2: free 🌟!");
-}
-
-fn transform(loop_size: usize, subject: usize) -> usize {
-    let mut value = 1;
-    for _ in 0..loop_size {
-        value *= subject;
-        value %= 20201227;
-    }
-    value
 }
