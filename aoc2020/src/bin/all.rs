44bc04889864930f6036c72d88e360256cfd7f76
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2020-12-26 00:38:28 +0000

    Pretty unicodes lol

--- aoc2020/src/bin/all.rs
+++ aoc2020/src/bin/all.rs
@@ -1,12 +1,8 @@
-use std::{ops::RangeInclusive, process::Command};
-
-const DAYS: RangeInclusive<usize> = 1..=25;
-
 fn main() {
-    println!("| Advent of Code 2020 |\n");
-    println!("Compiling...\n");
-    DAYS.for_each(|day| {
-        let cmd = Command::new("cargo").args(&["run", "--release", "--bin", &format!("day{:02}", day)]).output().unwrap();
+    println!("🎄 Advent of Code 2020 🎄\n");
+    println!("Compiling 🛠️\n");
+    (1..=25).for_each(|day| {
+        let cmd = std::process::Command::new("cargo").args(&["run", "--release", "--bin", &format!("day{:02}", day)]).output().unwrap();
         let out = String::from_utf8(cmd.stdout).unwrap();
         println!("> Day {}\n{}", day, out);
     })
