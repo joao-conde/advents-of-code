ee5ea1b54a89f617ae5ea6d9866db8463a9c1871
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-09-25 00:34:57 +0100

    feat: airbnb javascript prettier and eslint

--- aoc2018/.eslintrc.js
+++ aoc2018/.eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-    extends: ["eslint:recommended", "google"],
-    // rules: {
-    //     'no-console': 'off',
-    //     'max-len': 'off',
-    //     'no-tabs': 'off',
-    //     'indent': [2, "tab"],
-    //     'one-var': 'off',
-    //     'guard-for-in': 'off'
-    // },
-    // env: {
-    //     "node": true,
-    //     "es6": true
-    // }
+  extends: ['airbnb-base'],
 };
