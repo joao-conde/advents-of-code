05731eb4a7a77b8952493bc292299a9104fe2a03
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-09-25 02:35:48 +0100

    fix: remove non complete days

--- aoc2018/src/all.js
+++ aoc2018/src/all.js
@@ -2,7 +2,7 @@ const { execFileSync } = require("child_process");
 
 (async () => {
     console.log("🎄 Advent of Code 2018\n");
-    for (let d = 1; d <= 25; d += 1) {
+    for (let d = 1; d <= 9; d += 1) {
         console.log(`> Day ${d}`);
         const day = d >= 10 ? d : `0${d}`;
         execFileSync("node", [`src/day${day}.js`], {
