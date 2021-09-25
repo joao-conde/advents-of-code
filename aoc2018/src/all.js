9829261f6092520862a33727e65960d2f12e89f4
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-09-25 02:14:10 +0100

    feat: actually execute day scripts in all script

--- aoc2018/src/all.js
+++ aoc2018/src/all.js
@@ -1,7 +1,13 @@
-console.log("🎄 Advent of Code 2018\n");
-for (let d = 1; d <= 25; d += 1) {
-    console.log(`> Day ${d}`);
-    const day = d >= 10 ? d : `0${d}`;
-    console.log(`node src/day${day}.js`);
-    console.log();
-}
+const { execFileSync } = require("child_process");
+
+(async () => {
+    console.log("🎄 Advent of Code 2018\n");
+    for (let d = 1; d <= 25; d += 1) {
+        console.log(`> Day ${d}`);
+        const day = d >= 10 ? d : `0${d}`;
+        execFileSync("node", [`src/day${day}.js`], {
+            stdio: "inherit"
+        });
+        console.log();
+    }
+})();
