f0de74b102a60df96e5a060ff33bab836e4bfe95
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-11-27 14:48:45 +0000

    fix: just require

--- aoc2018/src/all.js
+++ aoc2018/src/all.js
@@ -1,11 +1,7 @@
-const { execFileSync } = require("child_process");
-
 console.log("🎄 Advent of Code 2018\n");
 for (let d = 1; d <= 9; d += 1) {
     console.log(`> Day ${d}`);
     const day = d >= 10 ? d : `0${d}`;
-    execFileSync("node", [`src/day${day}.js`], {
-        stdio: "inherit"
-    });
+    require(`./day${day}.js`);
     console.log();
 }
