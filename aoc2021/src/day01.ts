1a0cb3b98dc16ec5d2c94695d6082867c3a00b54
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-01 16:54:41 +0000

    refactor: shorten day01

--- aoc2021/src/day01.ts
+++ aoc2021/src/day01.ts
@@ -1,13 +1,12 @@
 import { readFileSync } from "fs";
 
-const sum = (array: number[]): number => array.reduce((acc, cur) => acc + cur);
+const sum = (xs: number[]): number => xs.reduce((sum, x) => sum + x);
 
-const countIncreases = (measures: number[]): number =>
-    measures.slice(1).filter((m, i) => m > measures[i]).length;
+const increases = (xs: number[]): number => xs.slice(1).filter((x, i) => x > xs[i]).length;
 
 const input = readFileSync("input/day01").toString();
 const measures = input.split("\n").map(x => parseInt(x));
-console.log("Part1: " + countIncreases(measures));
+console.log("Part1: " + increases(measures));
 
 const windowMeasures = measures.slice(0, -2).map((_, i) => sum(measures.slice(i, i + 3)));
-console.log("Part2: " + countIncreases(windowMeasures));
+console.log("Part2: " + increases(windowMeasures));
