bd5ad40f4c784365930a89d863022ab2f344ded4
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-01 13:01:43 +0000

    feat: day01 solution both parts

--- aoc2021/src/day01.ts
+++ aoc2021/src/day01.ts
@@ -1,4 +1,14 @@
 import { readFileSync } from "fs";
 
-const input = readFileSync("input/day01").toString();
-console.log(input);
+const input = readFileSync("input/day01").toString().split("\n");
+const measures = input.map(x => parseInt(x));
+
+const difs = measures.slice(1).map((m, i) => m - measures[i]);
+const increases = difs.filter(d => d > 0).length;
+console.log("Part1: " + increases);
+
+const windows = measures.map((m, i) => (i + 3 <= measures.length ? measures.slice(i, i + 3) : []));
+const windowMeasures = windows.map(w => w.reduce((acc, cur) => acc + cur, 0));
+const windowDifs = windowMeasures.slice(1).map((m, i) => m - windowMeasures[i]);
+const windowIncreases = windowDifs.filter(d => d > 0).length;
+console.log("Part2: " + windowIncreases);
