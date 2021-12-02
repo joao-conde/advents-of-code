0f27bcb9c33b0a84dc5a4ca5441e95a2d24aaacb
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-02 23:41:38 +0000

    refactor: simplify day2 solving both parts at once

--- 2021/src/day02.ts
+++ 2021/src/day02.ts
@@ -1,25 +1,22 @@
 import { readFileSync } from "fs";
 
-const directionTotal = (commands: [string, number][], command: string): number =>
-    commands.filter(([dir, _]) => dir === command).reduce((sum, [_, val]) => sum + val, 0);
-
 const input = readFileSync("input/day02").toString();
 const commands: [string, number][] = input
     .split("\n")
     .map(c => c.split(" "))
     .map(([dir, val]) => [dir, parseInt(val)]);
 
-const position = directionTotal(commands, "forward");
-const depth1 = directionTotal(commands, "down") - directionTotal(commands, "up");
-console.log("Part1: " + position * depth1);
-
-const [depth2, _] = commands.reduce(
-    ([depth, aim], [dir, val]) => {
-        if (dir === "forward") depth += aim * val;
+const [position, depth, aim] = commands.reduce(
+    ([position, depth, aim], [dir, val]) => {
+        if (dir === "forward") {
+            position += val;
+            depth += aim * val;
+        }
         if (dir === "down") aim += val;
         if (dir === "up") aim -= val;
-        return [depth, aim];
+        return [position, depth, aim];
     },
-    [0, 0]
+    [0, 0, 0]
 );
-console.log("Part2: " + position * depth2);
+console.log("Part1: " + position * aim);
+console.log("Part2: " + position * depth);
