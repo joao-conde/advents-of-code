8b85f2142dfb5d381c2f8fe8c37c99cf7191032b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-02 22:29:32 +0000

    refactor: day 1

--- 2021/src/day01.ts
+++ 2021/src/day01.ts
@@ -1,12 +1,13 @@
 import { readFileSync } from "fs";
 
-const sum = (xs: number[]): number => xs.reduce((sum, x) => sum + x);
-
 const increases = (xs: number[]): number => xs.slice(1).filter((x, i) => x > xs[i]).length;
 
 const input = readFileSync("input/day01").toString();
 const measures = input.split("\n").map(x => parseInt(x));
 console.log("Part1: " + increases(measures));
 
-const windowMeasures = measures.slice(0, -2).map((_, i) => sum(measures.slice(i, i + 3)));
+const windowMeasures = measures
+    .slice(0, -2)
+    .map((_, i) => measures.slice(i, i + 3))
+    .map(ms => ms.reduce((sum, m) => sum + m));
 console.log("Part2: " + increases(windowMeasures));
