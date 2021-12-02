b046acb4ec646e2f9d90fd3a32acdf58f7d2044d
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-02 21:48:30 +0000

    feat: day2 both parts

--- 2021/src/day02.ts
+++ 2021/src/day02.ts
@@ -1,6 +1,27 @@
 import { readFileSync } from "fs";
 
-const input = readFileSync("input/day02").toString();
+const directionTotal = (commands: [string, number][], command: string): number =>
+    commands.filter(([dir, _]) => dir === command).reduce((sum, [_, val]) => sum + val, 0);
 
-console.log("Part1: " + "");
-console.log("Part2: " + "");
+const input = readFileSync("input/day02").toString();
+const commands: [string, number][] = input
+    .split("\n")
+    .map(c => c.split(" "))
+    .map(([dir, val]) => [dir, parseInt(val)]);
+
+const position = directionTotal(commands, "forward");
+const downs = directionTotal(commands, "down");
+const ups = directionTotal(commands, "up");
+const depthP1 = downs - ups;
+console.log("Part1: " + depthP1 * position);
+
+const [depthP2, _] = commands.reduce(
+    ([depth, aim], [dir, val]) => {
+        if (dir === "forward") depth += aim * val;
+        if (dir === "down") aim += val;
+        if (dir === "up") aim -= val;
+        return [depth, aim];
+    },
+    [0, 0]
+);
+console.log("Part2: " + depthP2 * position);
