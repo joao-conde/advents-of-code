1688ff7048d2c719afc0a0bb9b651556d85812de
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-02 21:58:54 +0000

    feat: eslint allow unused var if prefixed by _

--- 2021/.eslintrc.js
+++ 2021/.eslintrc.js
@@ -7,5 +7,13 @@ module.exports = {
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "hive"
-    ]
+    ],
+    rules: {
+        "no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+        ]
+    },
+    ignorePatterns: ["node_modules/", "dist/"]
 };
