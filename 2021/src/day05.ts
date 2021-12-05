2e542348e9d988a4e66f6dcae2978d47080befd9
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-05 15:06:25 +0000

    rename to linepoints

--- 2021/src/day05.ts
+++ 2021/src/day05.ts
@@ -2,23 +2,20 @@ import { readFileSync } from "fs";
 
 import { range, scan } from "./utils";
 
-const generateCoords = (x1: number, y1: number, x2: number, y2: number): number[][] => {
+const linePoints = (x1: number, y1: number, x2: number, y2: number): number[][] => {
     const incx = x1 < x2 ? 1 : x1 > x2 ? -1 : 0;
     const incy = y1 < y2 ? 1 : y1 > y2 ? -1 : 0;
     const size = Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
-    const coords = scan(range(0, size), [x1, y1], ([x, y]) => [x + incx, y + incy]);
-    return coords;
+    return scan(range(0, size), [x1, y1], ([x, y]) => [x + incx, y + incy]);
 };
 
 const overlaps = (lines: number[][][], diagonals: boolean): number => {
     const overlaps = new Map<string, number>();
     lines.forEach(([[x1, y1], [x2, y2]]) => {
         if (!diagonals && x1 !== x2 && y1 !== y2) return;
-        generateCoords(x1, y1, x2, y2)
+        linePoints(x1, y1, x2, y2)
             .map(p => p.toString())
-            .forEach(p => {
-                overlaps.set(p, (overlaps.get(p) || 0) + 1);
-            });
+            .forEach(p => overlaps.set(p, (overlaps.get(p) || 0) + 1));
     });
     return Array.from(overlaps.values()).filter(x => x >= 2).length;
 };
