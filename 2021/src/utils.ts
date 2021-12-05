456cb2337a8d47dc1aba31d663870d04312cc66b
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-05 15:22:02 +0000

    refactor: change scan param name

--- 2021/src/utils.ts
+++ 2021/src/utils.ts
@@ -1,11 +1,11 @@
 export const range = (start: number, end: number, step = 1): number[] =>
     [...Array(end - start).keys()].map(i => start + i * step).filter(x => x < end);
 
-export const scan = <T, K>(iter: Iterable<K>, accum: T, fn: (state: T, next: K) => T): T[] => {
-    const states = [accum];
+export const scan = <T, K>(iter: Iterable<K>, seed: T, fn: (state: T, next: K) => T): T[] => {
+    const states = [seed];
     for (const k of iter) {
-        accum = fn(accum, k);
-        states.push(accum);
+        seed = fn(seed, k);
+        states.push(seed);
     }
     return states;
 };
