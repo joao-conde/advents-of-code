5ed9aae004873f40e0bab58cb04a052c0e14dc53
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-05 14:58:37 +0000

    refactor: day5 param destructoring

--- 2021/src/day05.ts
+++ 2021/src/day05.ts
@@ -6,17 +6,15 @@ const generateCoords = (x1: number, y1: number, x2: number, y2: number): number[
     const incx = x1 < x2 ? 1 : x1 > x2 ? -1 : 0;
     const incy = y1 < y2 ? 1 : y1 > y2 ? -1 : 0;
     const size = Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
-    const rn = range(0, size);
-    const coords = scan(rn, [x1, y1], ([x, y]) => [x + incx, y + incy]);
+    const coords = scan(range(0, size), [x1, y1], ([x, y]) => [x + incx, y + incy]);
     return coords;
 };
 
 const overlaps = (lines: number[][][], diagonals: boolean): number => {
     const overlaps = new Map<string, number>();
-    lines.forEach(l => {
-        if (!diagonals && l[0][0] !== l[1][0] && l[0][1] !== l[1][1]) return;
-        const coords = generateCoords(l[0][0], l[0][1], l[1][0], l[1][1]);
-        coords
+    lines.forEach(([[x1, y1], [x2, y2]]) => {
+        if (!diagonals && x1 !== x2 && y1 !== y2) return;
+        generateCoords(x1, y1, x2, y2)
             .map(p => p.toString())
             .forEach(p => {
                 overlaps.set(p, (overlaps.get(p) || 0) + 1);
