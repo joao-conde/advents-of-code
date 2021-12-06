c472afd10a3ed12c28b9262a10daa89111a2f528
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-06 14:19:07 +0000

    feat: day 6

---
+++ 2021/src/day06.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs";
+
+import { range, sum } from "./utils";
+
+const countFishes = (fishes: number[], days: number): number => {
+    const states = range(0, 10);
+    const counts = states.map(s => fishes.filter(f => f === s).length);
+    range(0, days).forEach(() => {
+        counts[9] += counts[0];
+        counts[7] += counts[0];
+        counts[0] = 0;
+        states.forEach(s => (counts[s] = counts[s + 1] || 0));
+    });
+    return sum(counts);
+};
+
+const fishes = readFileSync("input/day06")
+    .toString()
+    .split(",")
+    .map(x => parseInt(x));
+console.log("Part1:", countFishes(fishes, 80));
+console.log("Part2:", countFishes(fishes, 256));
