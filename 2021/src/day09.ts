4d342b2fc9b23347e6376f06cfccb1f34b0c2cf6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-10 00:01:04 +0000

    refactor: day9

--- 2021/src/day09.ts
+++ 2021/src/day09.ts
@@ -1,70 +1,74 @@
 import { readFileSync } from "fs";
 
-const basin = (
+import { mul, range, sum } from "./utils";
+
+const hash = (i: number, j: number): string => [i, j].toString();
+
+const isLowpoint = (heightmap: number[][], i: number, j: number): boolean => {
+    const up = (heightmap[i - 1] || [])[j];
+    const down = (heightmap[i + 1] || [])[j];
+    const left = heightmap[i][j - 1];
+    const right = heightmap[i][j + 1];
+    return [up, down, left, right].filter(x => x !== undefined).every(x => x > heightmap[i][j]);
+};
+
+const lowpoints = (heightmap: number[][]): number[][] =>
+    range(heightmap.length).reduce((points: number[][], i) => {
+        const lowpoints = range(heightmap[i].length)
+            .filter(j => isLowpoint(heightmap, i, j))
+            .map(j => [i, j]);
+        return [...points, ...lowpoints];
+    }, []);
+
+const fill = (
     heightmap: number[][],
     i: number,
     j: number,
-    basinsxd123: Set<string>
+    basins = new Set<string>()
 ): Set<string> => {
-    if (basinsxd123.has([i, j].toString())) return basinsxd123;
-
-    basinsxd123.add([i, j].toString());
+    if (basins.has(hash(i, j))) return basins;
 
     const lowpoint = heightmap[i][j];
+    basins.add(hash(i, j));
 
     // left
     for (let c = j - 1; c >= 0; c--) {
         if (heightmap[i][c] < lowpoint || heightmap[i][c] === 9) break;
-
-        basin(heightmap, i, c, basinsxd123);
-        basinsxd123.add([i, c].toString());
+        fill(heightmap, i, c, basins);
+        basins.add(hash(i, c));
     }
 
     // right
     for (let c = j + 1; c < heightmap[0].length; c++) {
         if (heightmap[i][c] < lowpoint || heightmap[i][c] === 9) break;
-        basin(heightmap, i, c, basinsxd123);
-        basinsxd123.add([i, c].toString());
+        fill(heightmap, i, c, basins);
+        basins.add(hash(i, c));
     }
 
     // up
     for (let r = i - 1; r >= 0; r--) {
         if (heightmap[r][j] < lowpoint || heightmap[r][j] === 9) break;
-        basin(heightmap, r, j, basinsxd123);
-        basinsxd123.add([r, j].toString());
+        fill(heightmap, r, j, basins);
+        basins.add(hash(r, j));
     }
 
     // down
     for (let r = i + 1; r < heightmap.length; r++) {
         if (heightmap[r][j] < lowpoint || heightmap[r][j] === 9) break;
-        basin(heightmap, r, j, basinsxd123);
-        basinsxd123.add([r, j].toString());
+        fill(heightmap, r, j, basins);
+        basins.add(hash(r, j));
     }
 
-    return basinsxd123;
+    return basins;
 };
 
 const input = readFileSync("input/day09").toString().split("\n");
 const heightmap = input.map(r => r.split("").map(h => parseInt(h)));
 
-let risk = 0;
-const lowpoints = [];
-for (let i = 0; i < heightmap.length; i++) {
-    for (let j = 0; j < heightmap[0].length; j++) {
-        const prevLine = heightmap[i - 1] || [];
-        const nextLine = heightmap[i + 1] || [];
-        const low = [prevLine[j], nextLine[j], heightmap[i][j - 1], heightmap[i][j + 1]]
-            .filter(x => x !== undefined)
-            .every(x => x > heightmap[i][j]);
+const points = lowpoints(heightmap);
+const risk = sum(points.map(([i, j]) => heightmap[i][j] + 1));
+console.log("Part1:", risk);
 
-        if (low) risk += heightmap[i][j] + 1;
-        if (low) lowpoints.push([i, j]);
-    }
-}
-
-console.log(risk);
-
-const basinSizes = lowpoints
-    .map(([i, j]) => basin(heightmap, i, j, new Set()).size)
-    .sort((a, b) => b - a);
-console.log(basinSizes[0] * basinSizes[1] * basinSizes[2]);
+const basins = points.map(([i, j]) => fill(heightmap, i, j));
+const sizes = basins.map(b => b.size).sort((a, b) => b - a);
+console.log("Part2:", mul(sizes.slice(0, 3)));
