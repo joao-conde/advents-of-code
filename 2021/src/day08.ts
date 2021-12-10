b0dac6ddeb724053a574dd6d8cd1cdc9e51fc5f6
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-10 00:04:37 +0000

    refactor: day8

--- 2021/src/day08.ts
+++ 2021/src/day08.ts
@@ -16,19 +16,19 @@ const buildMapper = (patterns: string[][]): string[][] => {
     const four = patterns.find(p => p.length === 4)!;
     const seven = patterns.find(p => p.length === 3)!;
     const eight = patterns.find(p => p.length === 7)!;
-    const missing = patterns
+    const sides234 = patterns
         .filter(p => p.length === 6)
         .flatMap(p => eight.filter(pi => !p.includes(pi)));
-    const side4 = missing.find(m => !four.includes(m))!;
+    const side4 = sides234.find(s => !four.includes(s))!;
     const nine = eight.filter(i => i !== side4);
-    const side2 = missing.find(m => one.includes(m));
+    const side2 = sides234.find(s => one.includes(s));
     const six = eight.filter(i => i !== side2);
-    const side3 = missing.find(m => m !== side4 && m !== side2);
+    const side3 = sides234.find(s => ![side2, side4].includes(s));
     const zero = eight.filter(i => i !== side3);
-    const five = eight.filter(i => i !== side2 && i !== side4);
+    const five = eight.filter(i => ![side2, side4].includes(i));
     const side5 = one.find(i => six.includes(i));
-    const side1 = four.find(i => i !== side2 && i !== side3 && i !== side5);
-    const three = eight.filter(i => i !== side1 && i !== side4);
+    const side1 = four.find(i => ![side2, side3, side5].includes(i));
+    const three = eight.filter(i => ![side1, side4].includes(i));
     const two = three.concat([side4]).filter(i => i !== side5);
     return [zero, one, two, three, four, five, six, seven, eight, nine];
 };
