b5d5d85e4e425b22bd42f8af7c82b78ee163699c
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-11 15:58:33 +0000

    refactor: day 11

--- 2021/src/day11.ts
+++ 2021/src/day11.ts
@@ -1,9 +1,24 @@
 import { readFileSync } from "fs";
 
-import { cartesian, sum } from "./utils";
+import { cartesian, copy, range } from "./utils";
 
 const GRID_SIZE = 10;
 const FLASH_THRESHOLD = 9;
+
+const step = (octopuses: number[][]): number => {
+    // increment octopuses energy level
+    cartesian(range(GRID_SIZE), range(GRID_SIZE)).forEach(([r, c]) => octopuses[r][c]++);
+
+    // increment adjacent octopuses energy and count flashers
+    let flashes = 0;
+    let flashers = getFlashers(octopuses);
+    while (flashers.length > 0) {
+        flashes += flashers.length;
+        flash(octopuses, flashers);
+        flashers = getFlashers(octopuses);
+    }
+    return flashes;
+};
 
 const getFlashers = (octopuses: number[][]): number[][] =>
     octopuses
@@ -25,33 +40,17 @@ const flash = (octopuses: number[][], flashers: number[][]) => {
     flashers.forEach(([i, j]) => (octopuses[i][j] = 0));
 };
 
-const step = (octopuses: number[][]): [number, number[][]] => {
-    // increment octopuses energy level
-    octopuses = octopuses.map(r => r.map(o => o + 1));
+const p1 = (octopuses: number[][]): number =>
+    range(100).reduce(flashes => flashes + step(octopuses), 0);
 
-    // increment adjacent octopuses energy
-    // and count all flashers
-    let flashes = 0;
-    let flashers = getFlashers(octopuses);
-    while (flashers.length > 0) {
-        flashes += flashers.length;
-        flash(octopuses, flashers);
-        flashers = getFlashers(octopuses);
-    }
-
-    return [flashes, octopuses];
+const p2 = (octopuses: number[][]): number => {
+    let i = 1;
+    while (step(octopuses) !== 100) i++;
+    return i;
 };
 
 const input = readFileSync("input/day11").toString().split("\n");
+const octopuses = input.map(r => r.split("").map(o => parseInt(o)));
 
-const flashes = [];
-let octopuses = input.map(r => r.split("").map(o => parseInt(o)));
-while (true) {
-    let newFlashes;
-    [newFlashes, octopuses] = step(octopuses);
-    flashes.push(newFlashes);
-    if (newFlashes === 100) break;
-}
-
-console.log("Part1:", sum(flashes.slice(0, 100)));
-console.log("Part2:", flashes.length);
+console.log("Part1:", p1(copy(octopuses)));
+console.log("Part2:", p2(octopuses));
