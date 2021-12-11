3a9274dca81cc181284d1177f40b71d2aa5e58e5
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-11 14:36:17 +0000

    refactor: day 11

--- 2021/src/day11.ts
+++ 2021/src/day11.ts
@@ -1,45 +1,38 @@
 import { readFileSync } from "fs";
 
-import { cartesian } from "./utils";
+import { cartesian, sum } from "./utils";
 
 const GRID_SIZE = 10;
+
+const flashersfn = (octopuses: number[][]): number[][] =>
+    octopuses
+        .map((r, i) => r.map((o, j) => [i, j, o]))
+        .flat()
+        .filter(([_i, _j, o]) => o > 9);
+
+const flash = (octopuses: number[][], flashers: number[][]) => {
+    flashers
+        .flatMap(([i, j, o]) => {
+            const cols = [-1, 0, 1].map(x => x + j).filter(c => c >= 0 && c < GRID_SIZE);
+            const rows = [-1, 0, 1].map(x => x + i).filter(r => r >= 0 && r < GRID_SIZE);
+            const coords = cartesian(rows, cols).filter(([r, c]) => r !== i || c !== j);
+            return coords;
+        })
+        .filter(([r, c]) => octopuses[r][c] !== 0)
+        .forEach(([r, c]) => octopuses[r][c]++);
+
+    flashers.forEach(([i, j]) => (octopuses[i][j] = 0));
+};
 
 const step = (octopuses: number[][]): [number, number[][]] => {
     octopuses = octopuses.map(r => r.map(o => o + 1));
 
     let flashes = 0;
-
-    let flashers = octopuses
-        .map((r, i) => r.map((o, j) => [i, j, o]))
-        .flat()
-        .filter(([_i, _j, o]) => o > 9);
-
+    let flashers = flashersfn(octopuses);
     while (flashers.length > 0) {
         flashes += flashers.length;
-
-        flashers.forEach(([i, j, o]) => {
-            const cols = [-1, 0, 1].map(x => x + j).filter(c => c >= 0 && c < GRID_SIZE);
-            const rows = [-1, 0, 1].map(x => x + i).filter(r => r >= 0 && r < GRID_SIZE);
-
-            const coords = [
-                ...new Set(cartesian(rows, cols).filter(([r, c]) => r !== i || c !== j))
-            ];
-
-            coords.forEach(([r, c]) => {
-                if (octopuses[r][c] !== 0) {
-                    octopuses[r][c]++;
-                }
-            });
-        });
-
-        flashers.forEach(([i, j]) => {
-            octopuses[i][j] = 0;
-        });
-
-        flashers = octopuses
-            .map((r, i) => r.map((o, j) => [i, j, o]))
-            .flat()
-            .filter(([_i, _j, o]) => o > 9);
+        flash(octopuses, flashers);
+        flashers = flashersfn(octopuses);
     }
 
     return [flashes, octopuses];
@@ -48,19 +41,12 @@ const step = (octopuses: number[][]): [number, number[][]] => {
 const input = readFileSync("input/day11").toString().split("\n");
 let octopuses = input.map(r => r.split("").map(o => parseInt(o)));
 
-let flashes = 0;
-for (let i = 0; i < 100; i++) {
+const flashes = [];
+while (true) {
     let newFlashes;
     [newFlashes, octopuses] = step(octopuses);
-    flashes += newFlashes;
+    flashes.push(newFlashes);
+    if (newFlashes === 100) break;
 }
-console.log("Part1:", flashes);
 
-octopuses = input.map(r => r.split("").map(o => parseInt(o)));
-flashes = 0;
-let i = 0;
-while (flashes !== 100) {
-    [flashes, octopuses] = step(octopuses);
-    i++;
-}
-console.log("Part2:", i);
+console.log(sum(flashes.slice(0, 100)), flashes.length);
