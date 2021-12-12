ed652c06468b17b2f93689a16de316c8f6008a6a
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-12 16:42:38 +0000

    style: slight prettify

--- 2021/src/day12.ts
+++ 2021/src/day12.ts
@@ -19,18 +19,14 @@ const dfs = (cur: string, curPath: string[] = [], paths: string[][] = []) => {
     const next = graph[cur];
 
     const isLowerCase = cur.toLowerCase() === cur;
-    if (isLowerCase && curPath.filter(c => c === cur).length >= 1) {
-        return;
-    }
+    if (isLowerCase && curPath.filter(c => c === cur).length >= 1) return;
 
     if (cur === "end") {
         paths.push(curPath.concat("end"));
         return;
     }
 
-    next.forEach(n => {
-        dfs(n, curPath.concat(cur), paths);
-    });
+    next.forEach(n => dfs(n, curPath.concat(cur), paths));
 };
 
 const dfs2 = (allow: boolean, cur: string, curPath: string[] = [], paths: string[][] = []) => {
@@ -43,18 +39,14 @@ const dfs2 = (allow: boolean, cur: string, curPath: string[] = [], paths: string
         allowthis = false;
     }
 
-    if (cur === "start" && curPath.includes("start")) {
-        return;
-    }
+    if (cur === "start" && curPath.includes("start")) return;
 
     if (cur === "end") {
         paths.push(curPath.concat("end"));
         return;
     }
 
-    next.forEach(n => {
-        dfs2(allowthis, n, curPath.concat(cur), paths);
-    });
+    next.forEach(n => dfs2(allowthis, n, curPath.concat(cur), paths));
 };
 
 const paths1: string[][] = [];
