44de7bc0248d2c3a2ff82b6b64fd51eab4c13f53
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-13 19:56:47 +0000

    refactor: move function up

--- 2021/src/day13.ts
+++ 2021/src/day13.ts
@@ -1,15 +1,5 @@
 import { range, readFileAsString, Set } from "./utils";
 
-const input = readFileAsString("input/day13").split("\n\n");
-const folds: [string, number][] = input[1]
-    .split("\n")
-    .map(l => l.split(" ")[2].split("="))
-    .map(([axis, val]) => [axis, parseInt(val)]);
-const dots = input[0]
-    .split("\n")
-    .map(l => l.split(",").map(x => parseInt(x)))
-    .reduce((dots, point) => dots.add(point as [number, number]), new Set<[number, number]>());
-
 const fold = (dots: Set<[number, number]>, [axis, value]: [string, number]) => {
     dots.values()
         .filter(([x, y]) => axis === "y" && y >= value)
@@ -20,9 +10,24 @@ const fold = (dots: Set<[number, number]>, [axis, value]: [string, number]) => {
         .forEach(([x, y]) => dots.delete([x, y]).add([2 * value - x, y]));
 };
 
+const input = readFileAsString("input/day13").split("\n\n");
+const folds: [string, number][] = input[1]
+    .split("\n")
+    .map(l => l.split(" ")[2].split("="))
+    .map(([axis, val]) => [axis, parseInt(val)]);
+const dots = input[0]
+    .split("\n")
+    .map(l => l.split(",").map(x => parseInt(x)))
+    .reduce((dots, point) => dots.add(point as [number, number]), new Set<[number, number]>());
+
 fold(dots, folds[0]);
 console.log("Part1:", dots.size());
 
-console.log("Part2:")
+console.log("Part2:");
 folds.slice(1).forEach(([axis, value]) => fold(dots, [axis, value]));
-range(6).map(x => console.log(range(40).map(y => dots.has([y, x]) ? "#" : " ").join("")))
+range(6).forEach(x => {
+    const row = range(40)
+        .map(y => (dots.has([y, x]) ? "#" : " "))
+        .join("");
+    console.log(row);
+});
