873fb6137a287193b193324072f4f16a362e1036
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-14 22:30:19 +0000

    tweaks

--- 2021/src/day14.ts
+++ 2021/src/day14.ts
@@ -1,6 +1,7 @@
 import { range, readFileAsString } from "./utils";
 
 const input = readFileAsString("input/day14").split("\n");
+const template = input[0].split("");
 const rules = input
     .slice(2)
     .map(r => r.split(" -> "))
@@ -9,61 +10,60 @@ const rules = input
         return rules;
     }, {});
 
-const chars = input[0].split("");
-const counts = chars.reduce((counts: Record<string, number>, c) => {
+const counts = template.reduce((counts: Record<string, number>, c) => {
     if (!(c in counts)) counts[c] = 0;
     counts[c]++;
     return counts;
 }, {});
-let pairs: Record<string, number> = chars
+let pairs: Record<string, number> = template
     .slice(0, -1)
-    .map((_, i) => chars.slice(i, i + 2).join(""))
+    .map((_, i) => template.slice(i, i + 2).join(""))
     .reduce((pairs: Record<string, number>, p) => {
         if (!(p in pairs)) pairs[p] = 0;
         pairs[p]++;
         return pairs;
     }, {});
 
-range(10).forEach(i => {
+range(10).forEach(_ => {
     const newpairs = Object.entries(pairs).reduce((newpairs: Record<string, number>, [k, v]) => {
         const letter = rules[k];
         const x = k[0] + letter;
         const y = letter + k[1];
 
         if (!(letter in counts)) counts[letter] = 0;
-        counts[letter] += pairs[k];
+        counts[letter] += v;
 
         if (!(x in newpairs)) newpairs[x] = 0;
         if (!(y in newpairs)) newpairs[y] = 0;
-        newpairs[x] += pairs[k];
-        newpairs[y] += pairs[k];
+        newpairs[x] += v;
+        newpairs[y] += v;
 
         return newpairs;
     }, {});
     pairs = newpairs;
 });
-const max = Object.keys(counts).reduce((max, c) => Math.max(max, counts[c]), 0);
-const min = Object.keys(counts).reduce((min, c) => Math.min(min, counts[c]), max);
+let max = Object.keys(counts).reduce((max, c) => Math.max(max, counts[c]), 0);
+let min = Object.keys(counts).reduce((min, c) => Math.min(min, counts[c]), max);
 console.log("Part1:", max - min);
 
-range(30).forEach(i => {
+range(30).forEach(_ => {
     const newpairs = Object.entries(pairs).reduce((newpairs: Record<string, number>, [k, v]) => {
         const letter = rules[k];
         const x = k[0] + letter;
         const y = letter + k[1];
 
         if (!(letter in counts)) counts[letter] = 0;
-        counts[letter] += pairs[k];
+        counts[letter] += v;
 
         if (!(x in newpairs)) newpairs[x] = 0;
         if (!(y in newpairs)) newpairs[y] = 0;
-        newpairs[x] += pairs[k];
-        newpairs[y] += pairs[k];
+        newpairs[x] += v;
+        newpairs[y] += v;
 
         return newpairs;
     }, {});
     pairs = newpairs;
 });
-const max2 = Object.keys(counts).reduce((max, c) => Math.max(max, counts[c]), 0);
-const min2 = Object.keys(counts).reduce((min, c) => Math.min(min, counts[c]), max2);
-console.log("Part1:", max2 - min2);
+max = Object.keys(counts).reduce((max, c) => Math.max(max, counts[c]), 0);
+min = Object.keys(counts).reduce((min, c) => Math.min(min, counts[c]), max);
+console.log("Part2:", max - min);
