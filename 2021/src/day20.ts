48cc794bdb020c51c85d176c8e89b7e6860e6b79
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-21 00:20:55 +0000

    cleanup

--- 2021/src/day20.ts
+++ 2021/src/day20.ts
@@ -1,9 +1,9 @@
-import { readFileAsString, Set as Myset } from "./utils";
-import { cartesian, range } from "./utils";
+import { readFileAsString, cartesian, range, Set } from "./utils";
 
-type Lights = Myset<[number, number]>;
+type Lights = Set<[number, number]>;
 
 const LIGHT = "#";
+const DARK = ".";
 
 const getBounds = (lights: Lights): { minx: number; miny: number; maxx: number; maxy: number } => {
     const minx = lights.values().reduce((minx, [x, y]) => (x < minx ? x : minx), Infinity);
@@ -13,7 +13,7 @@ const getBounds = (lights: Lights): { minx: number; miny: number; maxx: number; 
     return { minx, miny, maxx, maxy };
 };
 
-const enhance = (countDarks: boolean, algorithm: string, lights: Lights, darks: Lights): Lights => {
+const enhance = (algorithm: string, lights: Lights, space: Lights, spaceLit: boolean): Lights => {
     const { minx, miny, maxx, maxy } = getBounds(lights);
     const coords = cartesian(range(minx - 1, maxx + 2), range(miny - 1, maxy + 2));
 
@@ -28,9 +28,8 @@ const enhance = (countDarks: boolean, algorithm: string, lights: Lights, darks: 
             [x + 1, y - 1],
             [x + 1, y],
             [x + 1, y + 1]
-        ]
-        .reduce((bin, [x, y]) => {
-            const pixel = (lights.has([x, y]) || (countDarks && darks.has([x, y]))) ? "1" : "0";
+        ].reduce((bin, [x, y]) => {
+            const pixel = lights.has([x, y]) || (spaceLit && space.has([x, y])) ? "1" : "0";
             return bin + pixel;
         }, "");
 
@@ -40,7 +39,7 @@ const enhance = (countDarks: boolean, algorithm: string, lights: Lights, darks: 
         if (next === LIGHT) ls.add([x, y]);
 
         return ls;
-    }, new Myset() as Lights);
+    }, new Set() as Lights);
 };
 
 const [algorithm, image] = readFileAsString("input/day20").split("\n\n");
@@ -54,18 +53,18 @@ const lights = image
             .filter(([x, y, c]) => c === LIGHT)
     )
     .flat()
-    .reduce((lights, [x, y]) => lights.add([x, y]), new Myset()) as Lights;
+    .reduce((lights, [x, y]) => lights.add([x, y]), new Set()) as Lights;
 
-const darks = image
-.split("\n")
-.map((row, x) =>
-    row
-        .split("")
-        .map((c, y) => [x, y, c])
-        .filter(([x, y, c]) => c === ".")
-)
-.flat()
-.reduce((darks, [x, y]) => darks.add([x, y]), new Myset()) as Lights;
+const space = image
+    .split("\n")
+    .map((row, x) =>
+        row
+            .split("")
+            .map((c, y) => [x, y, c])
+            .filter(([x, y, c]) => c === DARK)
+    )
+    .flat()
+    .reduce((space, [x, y]) => space.add([x, y]), new Set()) as Lights;
 
-const lit = enhance(true, algorithm, enhance(false, algorithm, lights, darks), darks);
+const lit = enhance(algorithm, enhance(algorithm, lights, space, false), space, true);
 console.log("Part1:", lit.size());
