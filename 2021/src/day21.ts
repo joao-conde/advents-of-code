a1660c5765f4ceb3a6dfdff7e135384b39846c1d
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-23 21:34:53 +0000

    solution 21 explodes even with cache

--- 2021/src/day21.ts
+++ 2021/src/day21.ts
@@ -33,71 +33,82 @@ const play = (
 };
 
 type GameState = {
-    score1: number,
-    score2: number,
-    p1: number,
-    p2: number,
-    plays1: boolean,
-}
+    score1: number;
+    score2: number;
+    p1: number;
+    p2: number;
+    plays1: boolean;
+};
 
-const play2 = (
-    p1: number,
-    p2: number,
-): any => {
-    let states: GameState[] = [{
-        score1: 0,
-        score2: 0,
-        p1: p1,
-        p2: p2,
-        plays1: true,
-    }]
+const play2 = (p1: number, p2: number): any => {
+
+    const cache: Record<string, GameState> = {};
+
+    let states: GameState[] = [
+        {
+            score1: 0,
+            score2: 0,
+            p1: p1,
+            p2: p2,
+            plays1: true
+        }
+    ];
 
     let p1wins = 0;
     let p2wins = 0;
-    while(states.length > 0) {
+    while (states.length > 0) {
+        console.log(states.length)
         const nextStates: GameState[] = [];
 
-        states.forEach(({
-            score1,
-            score2,
-            p1,
-            p2,
-            plays1
-        }) => {
+        for (const state of states) {
+            const { score1, score2, p1, p2, plays1 } = state;
+
+            const k = JSON.stringify(state);
+            if(k in cache) {
+                const cached = cache[k]
+                if (cached.score1 > cached.score2) p1wins++;
+                else p2wins++;
+                continue;
+            }
+
             if (score1 >= 21 || score2 >= 21) {
                 if (score1 > score2) p1wins++;
                 else p2wins++;
-                return;
-            };
-            
-            const rolls = [1,2,3]
+
+                cache[k] = state
+
+                continue;
+            }
+
+            const rolls = [1, 2, 3];
             for (const r1 of rolls) {
-                for(const r2 of rolls) {
-                    for(const r3 of rolls) {
+                for (const r2 of rolls) {
+                    for (const r3 of rolls) {
                         const move = r1 + r2 + r3;
-                        const nextp1 = (p1 + move) % 10;
-                        const nextp2 = (p2 + move) % 10;
+                        const nextp1 = plays1 ? (p1 + move) % 10 : p1;
+                        const nextp2 = !plays1 ? (p2 + move) % 10 : p2;
+                        const nextscore1 = plays1 ? score1 + 1 + nextp1 : score1;
+                        const nextscore2 = !plays1 ? score2 + 1 + nextp2 : score2;
                         nextStates.push({
-                            score1: score1 + 1 + nextp1,
-                            score2: score2 + 1 + nextp2,
+                            score1: nextscore1,
+                            score2: nextscore2,
                             p1: nextp1,
                             p2: nextp2,
                             plays1: !plays1
-                        })
+                        });
                     }
                 }
             }
-        })
-        
+        }
         states = nextStates;
     }
 
-    return [p1wins, p2wins]
+    return [p1wins, p2wins];
 };
 
-const { score1, score2, rolls } = play(true, 4 - 1, 8 - 1, 0, 0, 1000); // -1 on coords
-console.log("Part1:", rolls * (score2 < score1 ? score2 : score1));
+// const { score1, score2, rolls } = play(true, 4 - 1, 8 - 1, 0, 0, 1000); // -1 on coords
+// console.log("Part1:", rolls * (score2 < score1 ? score2 : score1));
 
-const x = play2(3, 7)
-
-console.log(x)
+const x = play2(3, 7);
+console.log(x);
+console.log([444356092776315, 341960390180808]);
