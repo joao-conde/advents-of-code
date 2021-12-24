0bf953e78779bfd16b5df9a2523732f905d871a5
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-12-24 01:45:58 +0000

    refactor: day 21

--- 2021/src/day21.ts
+++ 2021/src/day21.ts
@@ -1,114 +1,77 @@
-// import { car } from "./utils";
-// const input = readFileAsString("input/day21");
+import { cartesian, range, readFileAsString } from "./utils";
 
-const play = (
-    plays1: boolean,
+type GameSummary = {
+    s1: number;
+    s2: number;
+    rolls?: number;
+};
+
+type Cache = Record<string, GameSummary>;
+
+type Die = (
     p1: number,
     p2: number,
-    score1: number,
-    score2: number,
-    max: number
-): { score1: number; score2: number; rolls: number } => {
-    let rolls = 0;
-    while (score1 < max && score2 < max) {
-        const move = (rolls % 100) + ((rolls + 1) % 100) + ((rolls + 2) % 100) + 3;
+    s1: number,
+    s2: number,
+    options: { rolls: number; cache: Cache }
+) => GameSummary;
 
-        if (plays1) {
-            p1 = (p1 + move) % 10;
-            score1 += p1 + 1;
-        } else {
-            p2 = (p2 + move) % 10;
-            score2 += p2 + 1;
-        }
-
-        plays1 = !plays1;
-        rolls += 3;
-    }
-
-    return {
-        score1,
-        score2,
-        rolls
-    };
+const play = (die: Die, p1: number, p2: number): GameSummary => {
+    const cache: Record<string, GameSummary> = {};
+    return die(p1 - 1, p2 - 1, 0, 0, { rolls: 0, cache: cache });
 };
 
-type GameState = {
-    score1: number;
-    score2: number;
-    p1: number;
-    p2: number;
-    plays1: boolean;
+const deterministic = (
+    p1: number,
+    p2: number,
+    s1: number,
+    s2: number,
+    { rolls }: { rolls: number }
+): GameSummary => {
+    if (s1 >= 1000 || s2 >= 1000) return { s1: s1, s2: s2, rolls: rolls };
+    const move = (rolls % 100) + ((rolls + 1) % 100) + ((rolls + 2) % 100) + 3;
+    const nextp1 = (p1 + move) % 10;
+    const nextscore1 = s1 + 1 + nextp1;
+    return deterministic(p2, nextp1, s2, nextscore1, { rolls: rolls + 3 });
 };
 
-const play2 = (p1: number, p2: number): any => {
+const quantum = (
+    p1: number,
+    p2: number,
+    s1: number,
+    s2: number,
+    { cache }: { cache: Cache }
+): GameSummary => {
+    if (s1 >= 21) return { s1: 1, s2: 0 };
+    if (s2 >= 21) return { s1: 0, s2: 1 };
 
-    const cache: Record<string, GameState> = {};
+    const k = `${p1};${p2};${s1};${s2}`;
+    if (k in cache) return cache[k];
 
-    let states: GameState[] = [
-        {
-            score1: 0,
-            score2: 0,
-            p1: p1,
-            p2: p2,
-            plays1: true
-        }
-    ];
+    const counts = cartesian(range(1, 4), range(1, 4), range(1, 4)).reduce(
+        (acc, [r1, r2, r3]) => {
+            const nextp1 = (p1 + r1 + r2 + r3) % 10;
+            const nextscore1 = s1 + 1 + nextp1;
+            const summary = quantum(p2, nextp1, s2, nextscore1, { cache: cache });
+            return { s1: acc.s1 + summary.s2, s2: acc.s2 + summary.s1 };
+        },
+        { s1: 0, s2: 0 }
+    ) as GameSummary;
 
-    let p1wins = 0;
-    let p2wins = 0;
-    while (states.length > 0) {
-        console.log(states.length)
-        const nextStates: GameState[] = [];
-
-        for (const state of states) {
-            const { score1, score2, p1, p2, plays1 } = state;
-
-            const k = JSON.stringify(state);
-            if(k in cache) {
-                const cached = cache[k]
-                if (cached.score1 > cached.score2) p1wins++;
-                else p2wins++;
-                continue;
-            }
-
-            if (score1 >= 21 || score2 >= 21) {
-                if (score1 > score2) p1wins++;
-                else p2wins++;
-
-                cache[k] = state
-
-                continue;
-            }
-
-            const rolls = [1, 2, 3];
-            for (const r1 of rolls) {
-                for (const r2 of rolls) {
-                    for (const r3 of rolls) {
-                        const move = r1 + r2 + r3;
-                        const nextp1 = plays1 ? (p1 + move) % 10 : p1;
-                        const nextp2 = !plays1 ? (p2 + move) % 10 : p2;
-                        const nextscore1 = plays1 ? score1 + 1 + nextp1 : score1;
-                        const nextscore2 = !plays1 ? score2 + 1 + nextp2 : score2;
-                        nextStates.push({
-                            score1: nextscore1,
-                            score2: nextscore2,
-                            p1: nextp1,
-                            p2: nextp2,
-                            plays1: !plays1
-                        });
-                    }
-                }
-            }
-        }
-        states = nextStates;
-    }
-
-    return [p1wins, p2wins];
+    cache[k] = counts;
+    return counts;
 };
 
-// const { score1, score2, rolls } = play(true, 4 - 1, 8 - 1, 0, 0, 1000); // -1 on coords
-// console.log("Part1:", rolls * (score2 < score1 ? score2 : score1));
+const REGEX = /Player \d starting position: (?<pos>\d+)/;
 
-const x = play2(3, 7);
-console.log(x);
-console.log([444356092776315, 341960390180808]);
+const input = readFileAsString("input/day21");
+const [p1, p2] = input
+    .split("\n")
+    .map(line => new RegExp(REGEX).exec(line)!.groups!.pos)
+    .map(pos => parseInt(pos)) as [number, number];
+
+let { s1, s2, rolls } = play(deterministic, p1, p2);
+console.log("Part1:", rolls! * (s2 < s1 ? s2 : s1));
+
+({ s1, s2, rolls } = play(quantum, p1, p2));
+console.log("Part2:", s2 > s1 ? s2 : s1);
