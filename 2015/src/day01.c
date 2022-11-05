499d621847fb93bd9ecc93841df644b518d36b94
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-11-05 22:03:43 +0000

    refactor: simplify one liner

--- 2015/src/day01.c
+++ 2015/src/day01.c
@@ -12,7 +12,7 @@ int main() {
     int floor = 0, position = -1;
     int nchars = strlen(input);
     for (int i = 0; i< nchars; i++) {
-        floor = floor + (input[i] == '(' ? 1 : -1);
+        floor += input[i] == '(' ? 1 : -1;
         if (position == -1 && floor == -1) position = i + 1;
     }
     free(input);
