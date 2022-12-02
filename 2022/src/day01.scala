11523becd8b97635433ccc07a268c2d567e7abeb
João Conde <joaodiasconde@gmail.com>
2022-12-02 01:21:36 +0000

    Update day01.scala

--- 2022/src/day01.scala
+++ 2022/src/day01.scala
@@ -4,7 +4,7 @@ import scala.util.Using
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day01"))(_.mkString).get
     val calories = input.split("\n\n").map(_.split("\n").map(_.toInt))
-    val top3 = calories.map(_.sum).sorted.reverse.slice(0, 3)
+    val top3 = calories.map(_.sum).sorted.reverse.take(3)
     println("Part1: " + top3(0))
     println("Part2: " + top3.sum)
 }
