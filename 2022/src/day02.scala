c77b3d5e9e68178712ca89a791775734f45d4562
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-03 00:21:27 +0000

    refactor: day2 2022

--- 2022/src/day02.scala
+++ 2022/src/day02.scala
@@ -1,22 +1,25 @@
 import scala.io.Source.fromFile
 import scala.util.Using
 
+val alias = Map("X" -> "A", "Y" -> "B", "Z" -> "C")
+val weakness = Map("A" -> "B", "B" -> "C", "C" -> "A")
+val points = Map("A" -> 1, "B" -> 2, "C" -> 3)
+
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day02"))(_.mkString).get
     val rounds = input.split("\n").map(_.split(" "))
-
-    val alias = Map("X" -> "A", "Y" -> "B", "Z" -> "C")
-    val weakness = Map("A" -> "B", "B" -> "C", "C" -> "A")
-    val points = Map("A" -> 1, "B" -> 2, "C" -> 3)
-
-    def score(x: String, y: String): Int =
-        points(y) + (if (x == y) 3 else if (weakness(y) != x) 6 else 0)
-
-    def shape(x: String, y: String): String =
-        if (y == "X") weakness.find(_._2 == x).map(_._1).get else if (y == "Y") x else weakness(x)
-
-    val p1 = rounds.map({ case Array(x, y) => score(x, alias(y)) }).sum
-    val p2 = rounds.map({ case Array(x, y) => score(x, shape(x, y)) }).sum
+    val p1 = rounds.map({ case Array(op, me) => scoreMe(alias(me), op) }).sum
+    val p2 = rounds.map({ case Array(op, strat) => scoreMe(move(op, strat), op) }).sum
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
+
+def scoreMe(me: String, op: String): Int =
+    points(me) + (if (op == me) 3 else if (weakness(me) != op) 6 else 0)
+
+def move(op: String, strategy: String): String =
+    strategy match {
+        case "X" => weakness.find(_._2 == op).map(_._1).get
+        case "Y" => op
+        case _   => weakness(op)
+    }
