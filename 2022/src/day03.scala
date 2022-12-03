c575af62fe332979015bdb132b41ad3fd241a5e1
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-03 15:26:11 +0000

    refactor: finaly found .sliding(x,x) <=> .chunks(x)

--- 2022/src/day03.scala
+++ 2022/src/day03.scala
@@ -5,12 +5,12 @@ def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day03"))(_.mkString).get
     val sacks = input.split("\n")
     val p1 = sacks
-        .map(s => (s.slice(0, s.length / 2), s.slice(s.length / 2, s.length)))
-        .map({ case (x, y) => x.find(c => y.contains(c)).get })
+        .map(s => s.sliding(s.length / 2, s.length / 2).toList)
+        .map(halves => halves(0).find(c => halves(1).contains(c)).get)
         .map(priority)
         .sum
-    val p2 = (0 until sacks.length / 3)
-        .map(i => sacks.slice(i * 3, i * 3 + 3))
+    val p2 = sacks
+        .sliding(3, 3)
         .map({ case Array(x, y, z) => x.find(c => y.contains(c) && z.contains(c)).get })
         .map(priority)
         .sum
