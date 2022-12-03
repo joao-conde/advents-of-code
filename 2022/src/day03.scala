f1e552c48be8192bd2789361b438d0cd93a034ca
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-03 14:50:49 +0000

    refactor: concise scala with some pattern matching day3

--- 2022/src/day03.scala
+++ 2022/src/day03.scala
@@ -4,24 +4,16 @@ import scala.util.Using
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day03"))(_.mkString).get
     val sacks = input.split("\n")
-
     val p1 = sacks
-        .map(s => {
-            val firstHalf = s.slice(0, s.length / 2)
-            val secondHalf = s.slice(s.length / 2, s.length)
-            val misplaced = firstHalf.find(c => secondHalf.contains(c)).get
-            priority(misplaced)
-        })
+        .map(s => (s.slice(0, s.length / 2), s.slice(s.length / 2, s.length)))
+        .map({ case (x, y) => x.find(c => y.contains(c)).get })
+        .map(priority)
         .sum
-
     val p2 = (0 until sacks.length / 3)
-        .map(i => {
-            val (s1, s2, s3) = (sacks(i * 3), sacks(i * 3 + 1), sacks(i * 3 + 2))
-            val badge = s1.find(c => s2.contains(c) && s3.contains(c)).get
-            priority(badge)
-        })
+        .map(i => sacks.slice(i * 3, i * 3 + 3))
+        .map({ case Array(x, y, z) => x.find(c => y.contains(c) && z.contains(c)).get })
+        .map(priority)
         .sum
-
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
