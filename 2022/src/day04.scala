db8326883302270e1ad4974675d29843083d85b6
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-04 16:46:09 +0000

    refactor: simplify day 4

--- 2022/src/day04.scala
+++ 2022/src/day04.scala
@@ -5,8 +5,8 @@ def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day04"))(_.mkString).get
     val pairs = input.split("\n").map(_.split(",").flatMap(_.split("-").map(_.toInt)))
     val ranges = pairs.map({ case Array(x, y, z, w) => (x to y, z to w) })
-    val p1 = ranges.count((r1, r2) => r1.intersect(r2) == r1 || r1.intersect(r2) == r2)
-    val p2 = ranges.count((r1, r2) => !r1.intersect(r2).isEmpty)
+    val p1 = ranges.count((r1, r2) => r1.forall(r2.contains) || r2.forall(r1.contains))
+    val p2 = ranges.count((r1, r2) => r1.exists(r2.contains))
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
