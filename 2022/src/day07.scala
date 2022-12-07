6bfc82647b2b0fd0a8b0574ae658c51c088e13b6
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-07 20:49:47 +0000

    refactor: style day7

--- 2022/src/day07.scala
+++ 2022/src/day07.scala
@@ -1,60 +1,70 @@
+import scala.collection.mutable.Map
 import scala.io.Source.fromFile
 import scala.util.Using
-import scala.collection.mutable.Map
 
+type INode = File | Dir;
 case class File(name: String, size: Int)
-case class Dir(name: String, parent: Option[Dir], var contents: Array[File | Dir])
-
-def findSizes(entry: Dir | File, sizes: Map[String, Int]): Int = {
-    entry match {
-        case File(_, size) => size
-        case Dir(name, _, contents) =>
-            contents.foldLeft(0)((acc, x) => {
-                val size = findSizes(x, sizes)
-                sizes(name) += size
-                acc + size
-            })
-    }
-}
+case class Dir(name: String, parent: Option[Dir], var children: Array[INode])
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day07"))(_.mkString).get
 
-    val dirs = Map[String, Dir]()
-    val commands = input.split("\n");
+    val root = buildFileSystem(input.split("\n"))
+    val sizes = computeSizes(root)
 
+    val p1 = sizes.values.filter(_ < 100000).sum
+    val p2 = sizes.values.toArray.sorted.find(x => 70000000 - sizes("/") + x >= 30000000).get
+    println("Part1: " + p1)
+    println("Part2: " + p2)
+}
+
+def buildFileSystem(commands: Array[String]): INode = {
     val cd = """\$ cd ([\w \/]+)""".r
     val dir = """dir (.+)""".r
     val file = """(\d+) (.+)""".r
 
     val root = Dir("/", None, Array())
-    dirs += "/" -> root
+    var cwd = root
+    commands.foreach(
+      {
+          case "$ cd /"  => cwd = root
+          case "$ cd .." => cwd = cwd.parent.getOrElse(root)
+          case cd(dir) =>
+              cwd = cwd.children
+                  .find({
+                      case (Dir(name, _, _)) => name == cwd.name + dir
+                      case _                 => false
+                  })
+                  .get
+                  .asInstanceOf[Dir]
+          case dir(name) => {
+              val child = Dir(cwd.name + name, Some(cwd), Array())
+              cwd.children = cwd.children :+ child
+          }
+          case file(size, name) => {
+              val child = File(name, size.toInt)
+              cwd.children = cwd.children :+ child
+          }
+          case _ =>
+      }
+    )
+    root
+}
 
-    var cur = root
-    commands.foreach(c => {
-        c match {
-            case "$ cd /"  => cur = root
-            case "$ cd .." => cur = cur.parent.getOrElse(root)
-            case cd(dir)   => cur = dirs(cur.name + dir)
-            case dir(name) => {
-                val child = Dir(cur.name + name, Some(cur), Array())
-                cur.contents = cur.contents :+ child
-                dirs += (cur.name + name) -> child
-            }
-            case file(size, name) => {
-                val child = File(name, size.toInt)
-                cur.contents = cur.contents :+ child
-            }
-            case _ =>
-        }
-    })
-
+def computeSizes(node: INode): Map[String, Int] = {
     val sizes = Map[String, Int]().withDefaultValue(0)
-    findSizes(root, sizes)
+    computeSizes(node, sizes)
+    sizes
+}
 
-    val p1 = sizes.values.filter(_ < 100000).sum
-    val unused = 70000000 - sizes("/")
-    val p2 = sizes.values.toArray.sorted.find(x => unused + x >= 30000000).get
-    println("Part1: " + p1)
-    println("Part2: " + p2)
+def computeSizes(node: INode, sizes: Map[String, Int]): Int = {
+    node match {
+        case File(_, size) => size
+        case Dir(name, _, children) =>
+            children.foldLeft(0)((acc, n) => {
+                val size = computeSizes(n, sizes)
+                sizes(name) += size
+                acc + size
+            })
+    }
 }
