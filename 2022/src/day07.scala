99cf38cb90c48cdfbdfc6994c89d24893992468f
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-07 20:59:26 +0000

    refactor: use lists instead of arrays

--- 2022/src/day07.scala
+++ 2022/src/day07.scala
@@ -4,7 +4,7 @@ import scala.util.Using
 
 type INode = File | Dir;
 case class File(name: String, size: Int)
-case class Dir(name: String, parent: Option[Dir], var children: Array[INode])
+case class Dir(name: String, parent: Option[Dir], var children: List[INode])
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day07"))(_.mkString).get
@@ -13,19 +13,20 @@ def main(args: Array[String]): Unit = {
     val sizes = computeSizes(root)
 
     val p1 = sizes.values.filter(_ < 100000).sum
-    val p2 = sizes.values.toArray.sorted.find(x => 70000000 - sizes("/") + x >= 30000000).get
+    val p2 = sizes.values.toArray.sorted.find(70000000 - sizes("/") + _ >= 30000000).get
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
 
-def buildFileSystem(commands: Array[String]): INode = {
+def buildFileSystem(lines: Array[String]): INode = {
     val cd = """\$ cd ([\w \/]+)""".r
     val dir = """dir (.+)""".r
     val file = """(\d+) (.+)""".r
 
-    val root = Dir("/", None, Array())
+    val root = Dir("/", None, List())
+
     var cwd = root
-    commands.foreach(
+    lines.foreach(
       {
           case "$ cd /"  => cwd = root
           case "$ cd .." => cwd = cwd.parent.getOrElse(root)
@@ -37,17 +38,12 @@ def buildFileSystem(commands: Array[String]): INode = {
                   })
                   .get
                   .asInstanceOf[Dir]
-          case dir(name) => {
-              val child = Dir(cwd.name + name, Some(cwd), Array())
-              cwd.children = cwd.children :+ child
-          }
-          case file(size, name) => {
-              val child = File(name, size.toInt)
-              cwd.children = cwd.children :+ child
-          }
-          case _ =>
+          case dir(name) => cwd.children = cwd.children :+ Dir(cwd.name + name, Some(cwd), List())
+          case file(size, name) => cwd.children = cwd.children :+ File(name, size.toInt)
+          case _                =>
       }
     )
+
     root
 }
 
