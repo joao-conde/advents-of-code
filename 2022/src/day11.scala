18ac72e7ca6a2e240bcbb6c621604e9a83eb1ffd
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-12-11 21:01:46 +0000

    refactor: parse input day 11

--- 2022/src/day11.scala
+++ 2022/src/day11.scala
@@ -2,35 +2,41 @@ import scala.io.Source.fromFile
 import scala.util.Using
 
 class MonkeyInTheMiddle(state: String) {
-    val monkeys = Array(
-      Monkey(List(83, 62, 93), old => old * 17, worry => if (worry % 2 == 0) 1 else 6),
-      Monkey(List(90, 55), old => old + 1, worry => if (worry % 17 == 0) 6 else 3),
-      Monkey(List(91, 78, 80, 97, 79, 88), old => old + 3, worry => if (worry % 19 == 0) 7 else 5),
-      Monkey(List(64, 80, 83, 89, 59), old => old + 5, worry => if (worry % 3 == 0) 7 else 2),
-      Monkey(List(98, 92, 99, 51), old => old * old, worry => if (worry % 5 == 0) 0 else 1),
-      Monkey(
-        List(68, 57, 95, 85, 98, 75, 98, 75),
-        old => old + 2,
-        worry => if (worry % 13 == 0) 4 else 0
-      ),
-      Monkey(List(74), old => old + 4, worry => if (worry % 7 == 0) 3 else 2),
-      Monkey(
-        List(68, 64, 60, 68, 87, 80, 82),
-        old => old * 19,
-        worry => if (worry % 11 == 0) 4 else 5
-      )
-    )
 
-    def play(rounds: Int, soother: BigInt => BigInt): BigInt = {
+    val re = """\s*Monkey .:
+\s*Starting\ items:\ (.+)
+\s*Operation: new = old (.) (.+)
+\s*Test: divisible by (\d+)
+\s*If true: throw to monkey (\d+)
+\s*If false: throw to monkey (\d+)""".r
+
+    val blocks = state.split("\n\n")
+
+    val lcm = blocks.map({ case re(_, _, _, prime, _, _) => prime.toInt }).reduce(_ * _)
+
+    val monkeys = blocks.map({
+        case re(items, operator, operand, prime, yes, no) => {
+            val itemList = items.split(",").map(s => BigInt(s.trim)).toList
+            val op = (operator, operand) match {
+                case ("*", "old") => (old: BigInt) => old * old
+                case ("*", _)     => (old: BigInt) => old * BigInt(operand)
+                case ("+", _)     => (old: BigInt) => old + BigInt(operand)
+            }
+            val test = (worry: BigInt) => if (worry % prime.toInt == 0) yes.toInt else no.toInt
+            Monkey(op, test, itemList)
+        }
+    })
+
+    def play(rounds: Int, soother: BigInt => BigInt = (old: BigInt) => old % lcm): BigInt = {
         (1 to rounds).foreach(r => monkeys.foreach(m => m.processItems(monkeys, soother)))
         monkeys.map(_.inspected).sorted.takeRight(2).product
     }
 }
 
 class Monkey(
-    var items: List[BigInt],
     op: BigInt => BigInt,
     test: BigInt => Int,
+    var items: List[BigInt],
     var inspected: BigInt = 0
 ) {
     def processItems(peers: Array[Monkey], soother: BigInt => BigInt): Unit = {
@@ -49,7 +55,7 @@ class Monkey(
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day11"))(_.mkString).get
     val p1 = MonkeyInTheMiddle(input).play(20, worry => worry / 3)
-    val p2 = MonkeyInTheMiddle(input).play(10000, worry => worry % 9699690)
+    val p2 = MonkeyInTheMiddle(input).play(10000)
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
