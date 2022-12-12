aca2539cc5791555b1300a8dd053558c0718d328
joao-conde <joaodiasconde@gmail.com>
2022-12-12 17:37:31 +0000

    feat: day 12 2022

--- 2022/src/day12.scala
+++ 2022/src/day12.scala
@@ -8,20 +8,19 @@ def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day12"))(_.mkString).get
 
     var heightmap = input.split("\n")
-
     val s0 = heightmap.indexWhere(_.contains('S'))
     val s1 = heightmap(s0).indexWhere(_ == 'S')
-    
     val e0 = heightmap.indexWhere(_.contains('E'))
     val e1 = heightmap(e0).indexWhere(_ == 'E')
-    
     heightmap = heightmap.map(r => r.replace("S", "a").replace("E", "z"))
     
-    val starts = heightmap.zipWithIndex.flatMap((r, i) => r.zipWithIndex.map((height, j) => (i, j, height)))
-            .filter((_, _, height) => height == 'a')
+    val starts = heightmap
+                    .zipWithIndex
+                    .flatMap((r, i) => r.zipWithIndex.map((height, j) => (i, j, height)))
+                    .filter((_, _, height) => height == 'a')
 
     val p1 = bfs(heightmap, (s0, s1), (e0, e1)).size
-    val p2 = starts.map((i, j, _) => bfs(heightmap, (i, j), (e0, e1)).size).min
+    val p2 = starts.map((i, j, _) => bfs(heightmap, (i, j), (e0, e1)).size).filter(_ > 0).min
     println("Part1: " + p1)
     println("Part2: " + p2)
 }
