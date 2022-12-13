ea563ca5db9f02e2845169ebf44f3f971996f9af
Joao Conde <joaodiasconde@gmail.com>
2022-12-13 17:31:05 +0000

    feat: part 2 d13 2022

--- 2022/src/day13.scala
+++ 2022/src/day13.scala
@@ -13,14 +13,19 @@ enum Comparison:
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day13"))(_.mkString).get
-
     val pairs = input.split("\n\n").map(_.split("\n").map(parse))
+
     val p1 = pairs.zipWithIndex
         .map({ case (Array(p0, p1), i) => if (compare(p0, p1) == Comparison.Lesser) i + 1 else 0 })
         .sum
 
+    val dividers = Array(parse("[[2]]"), parse("[[6]]"))
+    val sorted = (pairs :+ dividers).flatten
+        .sortWith((a, b) => compare(a, b) == Comparison.Lesser)
+    val p2 = (sorted.indexWhere(_ == dividers(0)) + 1) * (sorted.indexWhere(_ == dividers(1)) + 1)
+
     println(s"Part1: ${p1}")
-    println(s"Part2: ${}")
+    println(s"Part2: ${p2}")
 }
 
 def parse(str: String): List[Any] = {
