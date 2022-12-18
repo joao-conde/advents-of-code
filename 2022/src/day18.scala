57ec27b8379ec5cdf1d9d56b33a79d22ce716e5e
Joao Conde <joaodiasconde@gmail.com>
2022-12-18 18:57:06 +0000

    refactor: p1

--- 2022/src/day18.scala
+++ 2022/src/day18.scala
@@ -1,32 +1,27 @@
-// import scala.collection.mutable.{}
+import scala.collection.mutable.Map
 import scala.io.Source.fromFile
 import scala.math.abs
 import scala.util.Using
 
-case class Cube(x: Double, y: Double, z: Double)
 case class Side(x: Double, y: Double, z: Double)
+case class Cube(x: Double, y: Double, z: Double) {
+    val sides = List(
+      Side(x + 0.5, y, z),
+      Side(x - 0.5, y, z),
+      Side(x, y + 0.5, z),
+      Side(x, y - 0.5, z),
+      Side(x, y, z + 0.5),
+      Side(x, y, z - 0.5)
+    )
+}
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day18"))(_.mkString).get
-
     val cubes = input.split("\n").map(_.split(",").map(_.toInt)).map(c => Cube(c(0), c(1), c(2)))
 
-    val sides = cubes
-        .flatMap(adjacent)
-        .foldLeft(Map[Side, Int]().withDefaultValue(0))((sides, side) =>
-            sides.updated(side, sides(side) + 1)
-        )
-    val p1 = sides.count({ case (_, count) => count == 1 })
+    val sides: Map[Side, Int] = Map().withDefaultValue(0)
+    cubes.flatMap(_.sides).foreach(side => sides.update(side, sides(side) + 1))
 
+    val p1 = sides.count({ case (_, count) => count == 1 })
     println(s"Part1: $p1")
 }
-
-def adjacent(cube: Cube): List[Side] =
-    List(
-      Side(cube.x + 0.5, cube.y, cube.z),
-      Side(cube.x - 0.5, cube.y, cube.z),
-      Side(cube.x, cube.y + 0.5, cube.z),
-      Side(cube.x, cube.y - 0.5, cube.z),
-      Side(cube.x, cube.y, cube.z + 0.5),
-      Side(cube.x, cube.y, cube.z - 0.5)
-    )
