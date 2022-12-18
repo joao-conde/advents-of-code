949b0570fa810a6ab02843407f2fff07a8f4efa4
Joao Conde <joaodiasconde@gmail.com>
2022-12-18 20:08:24 +0000

    refactor: smarter way

--- 2022/src/day18.scala
+++ 2022/src/day18.scala
@@ -3,25 +3,21 @@ import scala.io.Source.fromFile
 import scala.math.abs
 import scala.util.Using
 
-case class Side(x: Double, y: Double, z: Double)
 case class Cube(x: Double, y: Double, z: Double) {
-    val sides = List(
-      Side(x + 0.5, y, z),
-      Side(x - 0.5, y, z),
-      Side(x, y + 0.5, z),
-      Side(x, y - 0.5, z),
-      Side(x, y, z + 0.5),
-      Side(x, y, z - 0.5)
+    def neighbors(): List[Cube] = List(
+      Cube(x + 1, y, z),
+      Cube(x - 1, y, z),
+      Cube(x, y + 1, z),
+      Cube(x, y - 1, z),
+      Cube(x, y, z + 1),
+      Cube(x, y, z - 1)
     )
 }
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day18"))(_.mkString).get
-    val cubes = input.split("\n").map(_.split(",").map(_.toInt)).map(c => Cube(c(0), c(1), c(2)))
-
-    val sides: Map[Side, Int] = Map().withDefaultValue(0)
-    cubes.flatMap(_.sides).foreach(side => sides.update(side, sides(side) + 1))
-
-    val p1 = sides.count({ case (_, count) => count == 1 })
-    println(s"Part1: $p1")
+    val cubes =
+        input.split("\n").map(_.split(",").map(_.toInt)).map(c => Cube(c(0), c(1), c(2))).toSet
+    val p1 = cubes.toList.map(cube => cube.neighbors().count(c => !cubes.contains(c))).sum
+    println(p1)
 }
