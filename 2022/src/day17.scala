5c0a2e3f1d0fa453c88682f1e9eba71ca13adda6
Joao Conde <joaodiasconde@gmail.com>
2022-12-18 13:34:45 +0000

    refactor: day17

--- 2022/src/day17.scala
+++ 2022/src/day17.scala
@@ -1,20 +1,23 @@
-import scala.collection.mutable.Set
+import scala.collection.mutable.{Map, Set}
 import scala.io.Source.fromFile
 import scala.math.max
 import scala.util.Using
 
 type Rock = Set[(Int, Int)]
+case class Config(heights: List[Int], rockIdx: Int, jetIdx: Int)
+case class State(step: Long, height: Int)
 
 class Chamber(input: String) {
-    val width = 7
+    val WIDTH = 7
+
     var height = 0
-    var solid = (0 until width).map(j => (0, j)).toSet
+    var terrain = Set((0 until WIDTH).map(j => (0, j)): _*)
 
     var jetIdx = 0
-    val jets = input.toList
+    val jets = input
 
     var rockIdx = 0
-    val rocks: List[Set[(Int, Int)]] = List(
+    val rocks = Array(
       Set((0, 0), (0, 1), (0, 2), (0, 3)),
       Set((0, 1), (1, 0), (1, 1), (1, 2), (2, 1)),
       Set((0, 0), (0, 1), (0, 2), (1, 2), (2, 2)),
@@ -22,103 +25,128 @@ class Chamber(input: String) {
       Set((0, 0), (0, 1), (1, 0), (1, 1))
     )
 
-    def topView(): String = {
-        val maxHeight = solid.map({ case (i, _) => i }).max
-        val maxHeights = (0 until width).map({ case j =>
-            solid.filter({ case (_, js) => j == js }).map({ case (i, _) => i }).max
-        })
-        maxHeights.map(h => h - maxHeight).toArray.mkString(",")
-    }
-
     def simulate(steps: Long): Long = {
-        // 7 ints heights, rockidx, jetidx
-        var cache: Map[(String, Int, Int), (Long, Long)] = Map()
-        var s: Long = 0
-        var additional: Long = 0
-        while (s < steps) {
+        // caches previous chamber configurations knowing
+        // if the same configuration appears, the height
+        // is going to increment in the same way next
+        // as it did from then until now
+        var cache: Map[Config, State] = Map()
+
+        // extra height added counter because directly changing
+        // the height of the chamber would yield incorrect
+        // computation of chamber configs
+        var extra = 0L
+
+        var curStep = 0L
+        while (curStep < steps) {
+            // one simulation step (drop one rock)
             step()
 
-            val config = (topView(), rockIdx, jetIdx)
-            if (cache.contains(config)) {
-                val (oldstep, oldheight) = cache.get(config).get
-                val repeat = (1000000000000L - s) / (s - oldstep)
-                s += (s - oldstep) * repeat
-                additional += (height - oldheight) * repeat
-
-                cache = Map()
-                // reset cache?
-            } else {
-                cache = cache + (config -> (s, height))
-                s += 1
+            val config = getConfig()
+            cache.get(config) match {
+                case Some(State(prevStep, prevHeight)) => {
+                    // compute how many times to repeat i.e.
+                    // how many steps to fast-forward
+                    val repeat = (steps - curStep) / (curStep - prevStep)
+                    extra += (height - prevHeight) * repeat
+                    curStep += (curStep - prevStep) * repeat
+                    cache.clear()
+                }
+                case _ => {
+                    val state = State(curStep, height)
+                    cache.addOne(config -> state)
+                    curStep += 1
+                }
             }
         }
-        height + additional
+
+        // returns current chamber height plus the
+        // height added with fast-forwards
+        height + extra
+    }
+
+    def getConfig(): Config = Config(normalizedHeights(), rockIdx, jetIdx)
+
+    def normalizedHeights(): List[Int] = {
+        // finds out the max height of each column
+        // and normalizes them by subtracting the tallest one
+        val maxHeights = (0 until WIDTH).map({ case j =>
+            terrain.filter({ case (_, tj) => j == tj }).map({ case (i, _) => i }).max
+        })
+        val maxHeight = maxHeights.max
+        maxHeights.map(h => h - maxHeight).toList
     }
 
     def step() = {
-        var rock = rocks(rockIdx).map({ case (i, j) => (i + height + 4, j + 2) })
-        rockIdx = (rockIdx + 1) % rocks.length
+        // build a new rock with current shape, offset by
+        // 4 units high and 2 to the right
+        var rock = nextRock()
 
-        var break = false
-        while (!break) {
+        // while the rock is not settled, the jet pushes
+        // it and the rock moves down, if possible
+        var settled = false
+        while (!settled) {
             rock = jetPush(rock)
 
             if (canMoveBottom(rock))
-                rock = rockMoveDown(rock)
+                rock = moveDown(rock)
             else
-                break = true
+                settled = true
         }
 
+        // update chamber max height tracker and add new
+        // settled rock as terrain
         height = max(height, rockHeight(rock))
-        rock.foreach(p => solid = solid + p)
+        terrain.addAll(rock)
+    }
+
+    def nextRock(): Rock = {
+        var rock = rocks(rockIdx).map({ case (i, j) => (i + height + 4, j + 2) })
+        rockIdx = (rockIdx + 1) % rocks.length
+        rock
+    }
+
+    def nextJet(): Char = {
+        val jet = jets(jetIdx)
+        jetIdx = (jetIdx + 1) % jets.length
+        jet
     }
 
     def jetPush(rock: Rock): Rock = {
-        val jet = jets(jetIdx)
-        jetIdx = (jetIdx + 1) % jets.length
-
+        val jet = nextJet()
         jet match {
-            case '<' if canMoveLeft(rock)  => rockMoveLeft(rock)
-            case '>' if canMoveRight(rock) => rockMoveRight(rock)
+            case '<' if canMoveLeft(rock)  => moveLeft(rock)
+            case '>' if canMoveRight(rock) => moveRight(rock)
             case _                         => rock
         }
     }
 
     def rockHeight(rock: Rock): Int = rock.map({ case (i, _) => i }).max
 
-    def canMoveLeft(rock: Rock): Boolean = {
-        rock.map({ case (_, j) => j }).min > 0
-        && rock.forall({ case (i, j) => !solid.contains((i, j - 1)) })
-    }
+    def canMoveLeft(rock: Rock): Boolean = rock.map({ case (_, j) => j }).min > 0 && rock.forall({
+        case (i, j) => !terrain.contains((i, j - 1))
+    })
 
-    def canMoveRight(rock: Rock): Boolean = {
-        rock.map({ case (_, j) => j }).max < width - 1
-        && rock.forall({ case (i, j) => !solid.contains((i, j + 1)) })
-    }
+    def canMoveRight(rock: Rock): Boolean =
+        rock.map({ case (_, j) => j }).max < WIDTH - 1 && rock.forall({ case (i, j) =>
+            !terrain.contains((i, j + 1))
+        })
 
-    def canMoveBottom(rock: Rock): Boolean = {
-        rock.forall({ case (i, j) => !solid.contains((i - 1, j)) })
-    }
+    def canMoveBottom(rock: Rock): Boolean = rock.forall({ case (i, j) =>
+        !terrain.contains((i - 1, j))
+    })
 
-    def rockAtBottom(rock: Rock): Boolean =
-        rock.exists({ case (i, j) => solid.contains((i - 1, j)) })
+    def moveLeft(rock: Rock): Rock = rock.map({ case (i, j) => (i, j - 1) })
 
-    def rockMoveLeft(rock: Rock): Rock =
-        rock.map({ case (i, j) => (i, j - 1) })
+    def moveRight(rock: Rock): Rock = rock.map({ case (i, j) => (i, j + 1) })
 
-    def rockMoveRight(rock: Rock): Rock =
-        rock.map({ case (i, j) => (i, j + 1) })
-
-    def rockMoveDown(rock: Rock): Rock =
-        rock.map({ case (i, j) => (i - 1, j) })
+    def moveDown(rock: Rock): Rock = rock.map({ case (i, j) => (i - 1, j) })
 }
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day17"))(_.mkString).get
-
     val p1 = Chamber(input).simulate(2022)
     val p2 = Chamber(input).simulate(1000000000000L) - 1
-
     println(s"Part1: ${p1}")
     println(s"Part2: ${p2}")
 }
