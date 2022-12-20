79152483938ce9e3ea6e271820cafe0200da5d49
Joao Conde <joaodiasconde@gmail.com>
2022-12-20 14:56:35 +0000

    fix: attempt to wrap negative numbers twice as much of the range

--- 2022/src/day20.scala
+++ 2022/src/day20.scala
@@ -4,8 +4,8 @@ import scala.util.Using
 
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day20"))(_.mkString).get
-
     val nums = input.split("\n").map(_.toInt)
+
     var indexes = (0 until nums.length).toList
     (0 until nums.length).foreach(i => {
         val num = nums(i)
@@ -15,6 +15,7 @@ def main(args: Array[String]): Unit = {
         indexes = front.filter(_ != i) ++ List(i) ++ back.filter(_ != i)
     })
 
+    println(indexes.map(nums(_)))
     val zero = indexes.indexWhere(nums(_) == 0)
     val p1 = List(1000, 2000, 3000).map(i => nums(indexes(wrap(i + zero, 0, nums.length - 1)))).sum
 
@@ -22,4 +23,11 @@ def main(args: Array[String]): Unit = {
     println(s"Part2: ${}")
 }
 
-def wrap(i: Int, from: Int, to: Int): Int = (if (i <= from) to - abs(from - i) else i) % (to + 1)
+def wrap(i: Int, from: Int, to: Int): Int = 
+    if (i > from) {
+        i % (to + 1)
+    } else {
+        val diff = abs(from - i)
+        val neg = diff % to
+        to - neg
+    }
