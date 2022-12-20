d13e3e94f966ac6ab233717baf75402f2ce112b2
Joao Conde <joaodiasconde@gmail.com>
2022-12-20 15:56:33 +0000

    fix: part1 with pure doubly linked list to avoid wrapping index clown fiesta

--- 2022/src/day20.scala
+++ 2022/src/day20.scala
@@ -2,32 +2,52 @@ import scala.io.Source.fromFile
 import scala.math.abs
 import scala.util.Using
 
+class Num(val value: Int, var prev: Option[Num], var next: Option[Num])
+
 def main(args: Array[String]): Unit = {
     val input = Using(fromFile("input/day20"))(_.mkString).get
-    val nums = input.split("\n").map(_.toInt)
 
-    var indexes = (0 until nums.length).toList
-    (0 until nums.length).foreach(i => {
-        val num = nums(i)
-        val at = indexes.indexWhere(_ == i)
-        val dst = wrap(at + num, 0, nums.length - 1)
-        val (front, back) = indexes.splitAt(dst + 1)
-        indexes = front.filter(_ != i) ++ List(i) ++ back.filter(_ != i)
+    val nums = input.split("\n").map(i => Num(i.toInt, None, None))
+    nums.zip(nums.drop(1))
+        .foreach((n1, n2) => {
+            n1.next = Some(n2)
+            n2.prev = Some(n1)
+        })
+    nums.last.next = Some(nums.head)
+    nums.head.prev = Some(nums.last)
+
+    nums.foreach(n => {
+        n.prev.get.next = n.next
+        n.next.get.prev = n.prev
+
+        val move = n.value
+
+        var (left, right) = (n.prev.get, n.next.get)
+        if (move >= 0)
+            (1 to move).foreach(_ => {
+                left = left.next.get
+                right = right.next.get
+            })
+        else
+            (1 to move.abs).foreach(_ => {
+                left = left.prev.get
+                right = right.prev.get
+            })
+
+        n.prev = Some(left)
+        n.next = Some(right)
+        left.next = Some(n)
+        right.prev = Some(n)
     })
 
-    println(indexes.map(nums(_)))
-    val zero = indexes.indexWhere(nums(_) == 0)
-    val p1 = List(1000, 2000, 3000).map(i => nums(indexes(wrap(i + zero, 0, nums.length - 1)))).sum
+    val zero = nums.find(_.value == 0).get
+    val p1 = List(1000, 2000, 3000)
+        .map(i => {
+            var cur = zero
+            (1 to i).foreach(_ => cur = cur.next.get)
+            cur.value
+        })
+        .sum
 
-    println(s"Part1: $p1")
-    println(s"Part2: ${}")
+    println(p1)
 }
-
-def wrap(i: Int, from: Int, to: Int): Int = 
-    if (i > from) {
-        i % (to + 1)
-    } else {
-        val diff = abs(from - i)
-        val neg = diff % to
-        to - neg
-    }
