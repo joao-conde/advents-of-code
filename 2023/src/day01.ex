b3b423ec0e69a00f1c0cb63621c864a2c21c1cdd
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-11-28 11:00:48 +0000

    feat: defmodule for elixir

--- 2023/src/day01.ex
+++ 2023/src/day01.ex
@@ -1,12 +1,18 @@
-calories =
-  "input/day01"
-  |> File.read!()
-  |> String.split("\n\n")
-  |> Enum.map(fn b -> b |> String.split() |> Enum.map(&String.to_integer/1) |> Enum.sum() end)
-  |> Enum.sort()
+defmodule Day01 do
+  def main do
+    calories = Day01.get_calories()
+    p1 = calories |> List.last()
+    p2 = calories |> Enum.take(-3) |> Enum.sum()
+    IO.puts("Part1: #{p1}")
+    IO.puts("Part2: #{p2}")
+  end
 
-p1 = calories |> List.last()
-p2 = calories |> Enum.take(-3) |> Enum.sum()
+  def get_calories() do
+    File.read!("input/day01")
+    |> String.split("\n\n")
+    |> Enum.map(fn b -> b |> String.split() |> Enum.map(&String.to_integer/1) |> Enum.sum() end)
+    |> Enum.sort()
+  end
+end
 
-IO.puts("Part1: #{p1}")
-IO.puts("Part2: #{p2}")
+Day01.main()
