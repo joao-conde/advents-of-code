3bffc12af0e860aaaa0335c8c6b70c02734ffc7e
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-04 22:14:50 +0000

    refactor: 2023 day04

--- 2023/src/bin/day04.rs
+++ 2023/src/bin/day04.rs
@@ -1,32 +1,27 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 fn main() {
     let input = std::fs::read_to_string("input/day04").unwrap();
 
     let card_matches: Vec<usize> = input.lines().map(card_matches).collect();
-    let scores: Vec<usize> = card_matches
-        .iter()
-        .map(|&matches| score_card(matches))
-        .collect();
-
-    let mut multipliers: Vec<usize> = vec![1; card_matches.len()];
+    let mut card_counts = vec![1; card_matches.len()];
     for (i, matches) in card_matches.iter().enumerate() {
         for j in i + 1..=i + matches {
-            if let Some(_) = multipliers.get(j) {
-                multipliers[j] += multipliers[i];
+            if card_counts.get(j).is_none() {
+                break;
             }
+            card_counts[j] += card_counts[i];
         }
     }
 
-    let p1: usize = scores.iter().sum();
-    let p2: usize = multipliers.iter().sum();
+    let p1: usize = card_matches.into_iter().map(card_score).sum();
+    let p2: usize = card_counts.iter().sum();
     println!("Part1: {p1}");
     println!("Part2: {p2}");
 }
 
 fn card_matches(line: &str) -> usize {
     let (_, numbers) = line.split_once(':').unwrap();
-
     let (winning, owned) = numbers.split_once('|').unwrap();
 
     let winning_set: HashSet<usize> = winning
@@ -43,7 +38,7 @@ fn card_matches(line: &str) -> usize {
     winning_set.intersection(&owned_set).count()
 }
 
-fn score_card(matches: usize) -> usize {
+fn card_score(matches: usize) -> usize {
     if matches > 0 {
         (1..matches).fold(1, |acc, _| acc * 2)
     } else {
