34ade5a85b5a9213019379294bfd911af0a7d9e8
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-07 15:42:30 +0000

    refactor: 2023 day7

--- 2023/src/bin/day07.rs
+++ 2023/src/bin/day07.rs
@@ -1,17 +1,12 @@
-use std::cmp::Ordering;
-// use std::sync::OnceLock;
-use std::{cmp::Reverse, collections::HashMap};
+use std::cmp::{Ordering, Reverse};
+use std::collections::HashMap;
 
-// static MEM: OnceLock<HashMap<usize, usize>> = OnceLock::new();
-
-#[derive(Debug)]
 struct Hand {
     cards: [Card; 5],
-    bid: usize,
     kind: HandKind,
+    bid: usize,
 }
 
-#[derive(Debug)]
 enum HandKind {
     Five(Card),
     Four(Card),
@@ -30,6 +25,92 @@ enum Card {
     Jack,
     Number(usize),
     Joker,
+}
+
+impl Hand {
+    fn cmp_strength(h1: &Hand, h2: &Hand) -> Ordering {
+        let s1 = h1.kind.strength();
+        let s2 = h2.kind.strength();
+        match s1.cmp(&s2) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => {
+                // find the first pair of cards in both hands
+                // that differs and compare that pair, if none
+                // compares the first pair (will return equal)
+                let (c1, c2) = h1
+                    .cards
+                    .iter()
+                    .zip(h2.cards.iter())
+                    .filter(|(c1, c2)| c1 != c2)
+                    .nth(0)
+                    .unwrap_or((&h1.cards[0], &h2.cards[0]));
+                c1.partial_cmp(c2).unwrap()
+            }
+        }
+    }
+}
+
+impl From<&str> for Hand {
+    fn from(line: &str) -> Self {
+        let (cards, bid) = line.split_once(' ').unwrap();
+        let cards = cards.chars().map(|c| c.into()).collect::<Vec<Card>>();
+        let cards = cards.try_into().unwrap();
+        let kind = HandKind::new(cards);
+        let bid = bid.parse().unwrap();
+        Hand { cards, kind, bid }
+    }
+}
+
+impl HandKind {
+    fn new(cards: [Card; 5]) -> HandKind {
+        let mut counts: HashMap<Card, usize> = cards.iter().fold(HashMap::new(), |mut map, c| {
+            *map.entry(*c).or_insert(0) += 1;
+            map
+        });
+
+        // removes the jokers from the card counts to be later
+        // added to the most frequent card count or returns
+        // immediately the best hand possible of five aces
+        let jokers = counts.remove(&Card::Joker).unwrap_or(0);
+        if jokers == 5 {
+            return HandKind::Five(Card::Ace);
+        }
+
+        // sorts the cards and counts by frequency and adds jokers
+        // to the most frequent to get the best possible hand
+        let mut counts: Vec<(Card, usize)> = counts.into_iter().collect();
+        counts.sort_by_key(|(_, cnt)| Reverse(*cnt));
+        counts[0].1 += jokers;
+
+        // determines the best hand kind and used cards based on the game rules
+        match (counts.get(0), counts.get(1)) {
+            (Some((c1, cnt1)), _) if *cnt1 == 5 => HandKind::Five(*c1),
+            (Some((c1, cnt1)), _) if *cnt1 == 4 => HandKind::Four(*c1),
+            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 3 && *cnt2 == 2 => {
+                HandKind::Full(*c1, *c2)
+            }
+            (Some((c1, cnt1)), _) if *cnt1 == 3 => HandKind::Three(*c1),
+            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 2 && *cnt2 == 2 => {
+                HandKind::TwoPair(*c1, *c2)
+            }
+            (Some((c1, cnt1)), _) if *cnt1 == 2 => HandKind::OnePair(*c1),
+            (Some((c1, _)), _) => HandKind::High(*c1),
+            _ => unreachable!(),
+        }
+    }
+
+    fn strength(&self) -> usize {
+        match self {
+            HandKind::Five(_) => 7,
+            HandKind::Four(_) => 6,
+            HandKind::Full(_, _) => 5,
+            HandKind::Three(_) => 4,
+            HandKind::TwoPair(_, _) => 3,
+            HandKind::OnePair(_) => 2,
+            HandKind::High(_) => 1,
+        }
+    }
 }
 
 impl Card {
@@ -64,108 +145,41 @@ impl PartialOrd for Card {
     }
 }
 
-impl Hand {
-    fn cmp_strength(h1: &Hand, h2: &Hand) -> Ordering {
-        match (h1.kind.strength(), h2.kind.strength()) {
-            (s1, s2) if s1 < s2 => Ordering::Less,
-            (s1, s2) if s1 > s2 => Ordering::Greater,
-            _ => {
-                let (c1, c2) = h1
-                    .cards
-                    .iter()
-                    .zip(h2.cards.iter())
-                    .filter(|(c1, c2)| c1 != c2)
-                    .nth(0)
-                    .unwrap();
-                c1.partial_cmp(c2).unwrap()
-            }
-        }
-    }
-}
-
-impl From<&str> for Hand {
-    fn from(line: &str) -> Self {
-        let (cards, bid) = line.split_once(' ').unwrap();
-        let cards = cards.chars().map(|c| c.into()).collect::<Vec<Card>>();
-        let cards = cards.try_into().unwrap();
-        let bid = bid.parse().unwrap();
-        let kind = HandKind::new(cards);
-        Hand { cards, bid, kind }
-    }
-}
-
-impl HandKind {
-    fn new(cards: [Card; 5]) -> HandKind {
-        let mut counts: HashMap<Card, usize> = cards.iter().fold(HashMap::new(), |mut map, c| {
-            *map.entry(*c).or_insert(0) += 1;
-            map
-        });
-
-        let jokers = counts.remove(&Card::Joker).unwrap_or(0);
-
-        let mut counts: Vec<(Card, usize)> = counts.into_iter().collect();
-        counts.sort_by_key(|(_, cnt)| Reverse(*cnt));
-
-        if counts.get(0).is_some() {
-            counts[0].1 += jokers
-        }
-
-        match (counts.get(0), counts.get(1)) {
-            (Some((c1, cnt1)), _) if *cnt1 == 5 => HandKind::Five(*c1),
-            (Some((c1, cnt1)), _) if *cnt1 == 4 => HandKind::Four(*c1),
-            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 3 && *cnt2 == 2 => {
-                HandKind::Full(*c1, *c2)
-            }
-            (Some((c1, cnt1)), _) if *cnt1 == 3 => HandKind::Three(*c1),
-            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 2 && *cnt2 == 2 => {
-                HandKind::TwoPair(*c1, *c2)
-            }
-            (Some((c1, cnt1)), _) if *cnt1 == 2 => HandKind::OnePair(*c1),
-            (Some((c1, _)), _) => HandKind::High(*c1),
-            (None, _) => HandKind::Five(Card::Ace),
-        }
-    }
-
-    fn strength(&self) -> usize {
-        match self {
-            HandKind::Five(_) => 7,
-            HandKind::Four(_) => 6,
-            HandKind::Full(_, _) => 5,
-            HandKind::Three(_) => 4,
-            HandKind::TwoPair(_, _) => 3,
-            HandKind::OnePair(_) => 2,
-            HandKind::High(_) => 1,
-        }
-    }
-}
-
 fn main() {
     let input = std::fs::read_to_string("input/day07").unwrap();
 
-    let mut hands: Vec<Hand> = input.lines().map(|l| l.into()).collect();
-    hands.sort_by(Hand::cmp_strength);
+    let mut regular_hands: Vec<Hand> = input.lines().map(|l| l.into()).collect();
+    regular_hands.sort_by(Hand::cmp_strength);
 
-    let p1: usize = hands.iter().enumerate().map(|(i, h)| (i + 1) * h.bid).sum();
-    println!("Part1: {p1}");
-
-    let mut hands: Vec<Hand> = hands
-        .into_iter()
-        .map(|h| {
-            let cards = h.cards.map(|c| match c {
+    let mut joker_hands: Vec<Hand> = input
+        .lines()
+        .map(|l| {
+            let hand = Hand::from(l);
+            let cards = hand.cards.map(|c| match c {
                 Card::Jack => Card::Joker,
                 card => card,
             });
             let kind = HandKind::new(cards);
-
             Hand {
                 cards,
                 kind,
-                bid: h.bid,
+                bid: hand.bid,
             }
         })
         .collect();
-    hands.sort_by(Hand::cmp_strength);
+    joker_hands.sort_by(Hand::cmp_strength);
 
-    let p2: usize = hands.iter().enumerate().map(|(i, h)| (i + 1) * h.bid).sum();
+    let p1: usize = regular_hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) * h.bid)
+        .sum();
+    println!("Part1: {p1}");
+
+    let p2: usize = joker_hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) * h.bid)
+        .sum();
     println!("Part2: {p2}");
 }
