851f39075a46ccc0a51f977a80bebf133962c108
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-07 12:29:30 +0000

    feat: 2023 day7 p1

---
+++ 2023/src/bin/day07.rs
@@ -0,0 +1,142 @@
+use std::cmp::Ordering;
+// use std::sync::OnceLock;
+use std::{cmp::Reverse, collections::HashMap};
+
+// static MEM: OnceLock<HashMap<usize, usize>> = OnceLock::new();
+
+#[derive(Debug)]
+struct Hand {
+    cards: [Card; 5],
+    bid: usize,
+    kind: HandKind,
+}
+
+#[derive(Debug)]
+enum HandKind {
+    Five(Card),
+    Four(Card),
+    Full(Card, Card),
+    Three(Card),
+    TwoPair(Card, Card),
+    OnePair(Card),
+    High(Card),
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd)]
+enum Card {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Number(usize),
+}
+
+impl Card {
+    fn strength(&self) -> usize {
+        match self {
+            Self::Ace => 14,
+            Self::King => 13,
+            Self::Queen => 12,
+            Self::Jack => 11,
+            Self::Number(n) => *n,
+        }
+    }
+}
+
+impl From<char> for Card {
+    fn from(value: char) -> Self {
+        match value {
+            'A' => Self::Ace,
+            'K' => Self::King,
+            'Q' => Self::Queen,
+            'J' => Self::Jack,
+            'T' => Self::Number(10),
+            n => Self::Number(n.to_digit(10).unwrap() as usize),
+        }
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.strength().cmp(&other.strength())
+    }
+}
+
+impl Hand {
+    fn cmp_strength(h1: &Hand, h2: &Hand) -> Ordering {
+        match (h1.kind.strength(), h2.kind.strength()) {
+            (s1, s2) if s1 < s2 => Ordering::Less,
+            (s1, s2) if s1 > s2 => Ordering::Greater,
+            _ => {
+                let (c1, c2) = h1
+                    .cards
+                    .iter()
+                    .zip(h2.cards.iter())
+                    .filter(|(c1, c2)| c1 != c2)
+                    .nth(0)
+                    .unwrap();
+                c1.cmp(c2)
+            }
+        }
+    }
+}
+
+impl From<&str> for Hand {
+    fn from(line: &str) -> Self {
+        let (cards, bid) = line.split_once(' ').unwrap();
+        let cards = cards.chars().map(|c| c.into()).collect::<Vec<Card>>();
+        let cards = cards.try_into().unwrap();
+        let bid = bid.parse().unwrap();
+        let kind = HandKind::new(cards);
+        Hand { cards, bid, kind }
+    }
+}
+
+impl HandKind {
+    fn new(cards: [Card; 5]) -> HandKind {
+        let counts: HashMap<Card, usize> = cards.iter().fold(HashMap::new(), |mut map, c| {
+            *map.entry(*c).or_insert(0) += 1;
+            map
+        });
+
+        let mut counts: Vec<(Card, usize)> = counts.into_iter().collect();
+        counts.sort_by_key(|(_, cnt)| Reverse(*cnt));
+
+        match (counts.get(0), counts.get(1)) {
+            (Some((c1, cnt1)), _) if *cnt1 == 5 => HandKind::Five(*c1),
+            (Some((c1, cnt1)), _) if *cnt1 == 4 => HandKind::Four(*c1),
+            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 3 && *cnt2 == 2 => {
+                HandKind::Full(*c1, *c2)
+            }
+            (Some((c1, cnt1)), _) if *cnt1 == 3 => HandKind::Three(*c1),
+            (Some((c1, cnt1)), Some((c2, cnt2))) if *cnt1 == 2 && *cnt2 == 2 => {
+                HandKind::TwoPair(*c1, *c2)
+            }
+            (Some((c1, cnt1)), _) if *cnt1 == 2 => HandKind::OnePair(*c1),
+            (Some((c1, _)), _) => HandKind::High(*c1),
+            _ => unreachable!(),
+        }
+    }
+
+    fn strength(&self) -> usize {
+        match self {
+            HandKind::Five(_) => 7,
+            HandKind::Four(_) => 6,
+            HandKind::Full(_, _) => 5,
+            HandKind::Three(_) => 4,
+            HandKind::TwoPair(_, _) => 3,
+            HandKind::OnePair(_) => 2,
+            HandKind::High(_) => 1,
+        }
+    }
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input/day07").unwrap();
+
+    let mut hands: Vec<Hand> = input.lines().map(|l| l.into()).collect();
+    hands.sort_by(Hand::cmp_strength);
+
+    let p1: usize = hands.iter().enumerate().map(|(i, h)| (i + 1) * h.bid).sum();
+    println!("Part1: {p1}");
+}
