e26c03f2406962792e80e0c22c0c64ccfacaa7ad
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-15 23:32:38 +0000

    refactor: 2023 day15

--- 2023/src/bin/day15.rs
+++ 2023/src/bin/day15.rs
@@ -4,16 +4,26 @@ fn main() {
     let input = std::fs::read_to_string("input/day15").unwrap();
     let steps: Vec<&str> = input.split(',').collect();
 
+    let (boxes, lengths) = fill_boxes(&steps);
+
     let p1: usize = steps.iter().map(|s| ha256(s)).sum();
     println!("Part1: {p1}");
 
-    let mut boxes: Vec<Vec<&str>> = vec![vec![]; 256];
-    let mut focal_lengths: HashMap<&str, usize> = HashMap::new();
+    let p2: usize = lengths
+        .keys()
+        .map(|label| focus_power(label, &boxes, &lengths))
+        .sum();
+    println!("Part2: {p2}");
+}
 
-    for step in &steps {
+fn fill_boxes<'a>(steps: &[&'a str]) -> (Vec<Vec<&'a str>>, HashMap<&'a str, usize>) {
+    let mut boxes: Vec<Vec<&str>> = vec![vec![]; 256];
+    let mut lengths: HashMap<&str, usize> = HashMap::new();
+
+    for step in steps {
         if let Some((label, length)) = step.split_once('=') {
             let length: usize = length.parse().unwrap();
-            focal_lengths.insert(label, length);
+            lengths.insert(label, length);
 
             let box_i = ha256(label);
             if !boxes[box_i].contains(&label) {
@@ -22,22 +32,22 @@ fn main() {
         } else if let Some((label, _)) = step.split_once('-') {
             let box_i = ha256(label);
             boxes[box_i].retain(|l| l != &label);
-            focal_lengths.remove(label);
+            lengths.remove(label);
         }
     }
 
-    let mut sum = 0;
-    for label in focal_lengths.keys() {
-        let box_i = ha256(label);
-        let slot = boxes[box_i].iter().position(|l| l == label).unwrap();
-        let focal_length = focal_lengths[label];
-        sum += (box_i + 1) * (slot + 1) * focal_length;
-    }
-    println!("Part2: {sum}");
+    (boxes, lengths)
 }
 
 fn ha256(value: &str) -> usize {
     value
         .chars()
         .fold(0, |hash, c| ((hash + c as usize) * 17) % 256)
+}
+
+fn focus_power(label: &str, boxes: &[Vec<&str>], focal_lengths: &HashMap<&str, usize>) -> usize {
+    let box_i = ha256(label);
+    let slot = boxes[box_i].iter().position(|l| l == &label).unwrap();
+    let focal_length = focal_lengths[label];
+    (box_i + 1) * (slot + 1) * focal_length
 }
