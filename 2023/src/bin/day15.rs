08bf3d6f1898c605d7e7bc54e84e9bd8314948c1
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-15 23:19:14 +0000

    feat: 2023 day 15

---
+++ 2023/src/bin/day15.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = std::fs::read_to_string("input/day15").unwrap();
+    let steps: Vec<&str> = input.split(',').collect();
+
+    let p1: usize = steps.iter().map(|s| ha256(s)).sum();
+    println!("Part1: {p1}");
+
+    let mut boxes: Vec<Vec<&str>> = vec![vec![]; 256];
+    let mut focal_lengths: HashMap<&str, usize> = HashMap::new();
+
+    for step in &steps {
+        if let Some((label, length)) = step.split_once('=') {
+            let length: usize = length.parse().unwrap();
+            focal_lengths.insert(label, length);
+
+            let box_i = ha256(label);
+            if !boxes[box_i].contains(&label) {
+                boxes[box_i].push(label);
+            }
+        } else if let Some((label, _)) = step.split_once('-') {
+            let box_i = ha256(label);
+            boxes[box_i].retain(|l| l != &label);
+            focal_lengths.remove(label);
+        }
+    }
+
+    let mut sum = 0;
+    for label in focal_lengths.keys() {
+        let box_i = ha256(label);
+        let slot = boxes[box_i].iter().position(|l| l == label).unwrap();
+        let focal_length = focal_lengths[label];
+        sum += (box_i + 1) * (slot + 1) * focal_length;
+    }
+    println!("Part2: {sum}");
+}
+
+fn ha256(value: &str) -> usize {
+    value
+        .chars()
+        .fold(0, |hash, c| ((hash + c as usize) * 17) % 256)
+}
