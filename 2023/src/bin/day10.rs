4135a7374991bb0adeea1e51e8e6d2c8c9216c2e
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-15 00:49:31 +0000

    feat: 2023 day10 p2

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -19,18 +19,19 @@ fn main() {
         .unwrap();
 
     let mut deque: VecDeque<(usize, usize, usize)> = VecDeque::new();
-
     let mut visited: HashSet<(usize, usize)> = HashSet::new();
 
     deque.push_back((si, sj, 0));
 
     let mut max_steps = 0;
+
     while let Some((i, j, distance)) = deque.pop_front() {
         if visited.contains(&(i, j)) {
             continue;
         }
 
         visited.insert((i, j));
+
         max_steps = max_steps.max(distance);
 
         let cur_tile = grid[i][j];
@@ -53,5 +54,36 @@ fn main() {
         }
     }
 
-    println!("{max_steps}");
+    println!("p1: {max_steps}");
+
+    let x: usize = (0..nrows).map(|i| inside_loop(&grid, &visited, i)).sum();
+    println!("p2: {x}");
+
+    inside_loop(&grid, &visited, 2);
+}
+
+fn inside_loop(grid: &[Vec<char>], visited: &HashSet<(usize, usize)>, i: usize) -> usize {
+    let mut counts = 0;
+    let mut inside = false;
+
+    for (j, &tile) in grid[i].iter().enumerate() {
+        if visited.contains(&(i, j)) {
+            inside = match tile {
+                '|' => !inside,
+                'J' => !inside,
+                '-' => inside,
+                'L' => !inside,
+                '7' => inside,
+                'F' => inside,
+                'S' => inside,
+                _ => unreachable!(),
+            }
+        };
+
+        if inside && !visited.contains(&(i, j)) {
+            counts += 1;
+        }
+    }
+
+    counts
 }
