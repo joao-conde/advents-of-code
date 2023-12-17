65653980a063f3afb46985eb542642095049ba60
João Conde <16060539+joao-conde@users.noreply.github.com>
2023-12-17 16:19:29 +0000

    refactor: 2023 day10

--- 2023/src/bin/day10.rs
+++ 2023/src/bin/day10.rs
@@ -2,30 +2,30 @@ use std::collections::{HashSet, VecDeque};
 
 fn main() {
     let input = std::fs::read_to_string("input/day10").unwrap();
-    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let tiles: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
 
-    let (loop_tiles, p1) = find_loop_tiles(&grid);
+    let (loop_tiles, p1) = find_loop_tiles(&tiles);
     println!("Part1: {p1}");
 
-    let p2: usize = grid
+    let p2: usize = tiles
         .iter()
         .enumerate()
-        .map(|(i, _)| count_enclosed_tiles(&grid, &loop_tiles, i))
+        .map(|(i, _)| count_enclosed_tiles(&tiles, &loop_tiles, i))
         .sum();
     println!("Part2: {p2}");
 }
 
-fn find_loop_tiles(grid: &[Vec<char>]) -> (HashSet<(usize, usize)>, usize) {
+fn find_loop_tiles(tiles: &[Vec<char>]) -> (HashSet<(usize, usize)>, usize) {
     // tiles and the connections they can receive
     let north_conns = ['|', 'L', 'J', 'S'];
     let south_conns = ['|', '7', 'F', 'S'];
     let west_conns = ['-', 'J', '7', 'S'];
     let east_conns = ['-', 'L', 'F', 'S'];
 
-    // grid dimensions and starting point
-    let nrows = grid.len();
-    let ncols = grid[0].len();
-    let (si, sj) = grid
+    // field dimensions and starting point
+    let nrows = tiles.len();
+    let ncols = tiles[0].len();
+    let (si, sj) = tiles
         .iter()
         .enumerate()
         .find_map(|(i, row)| row.iter().position(|&c| c == 'S').map(|j| (i, j)))
@@ -46,22 +46,25 @@ fn find_loop_tiles(grid: &[Vec<char>]) -> (HashSet<(usize, usize)>, usize) {
         visited.insert((i, j));
         max_steps = max_steps.max(distance);
 
-        let cur_tile = grid[i][j];
+        let cur_tile = tiles[i][j];
 
-        if i > 0 && north_conns.contains(&cur_tile) && south_conns.contains(&grid[i - 1][j]) {
+        if i > 0 && north_conns.contains(&cur_tile) && south_conns.contains(&tiles[i - 1][j]) {
             deque.push_back((i - 1, j, distance + 1));
         }
 
-        if i + 1 < nrows && south_conns.contains(&cur_tile) && north_conns.contains(&grid[i + 1][j])
+        if i + 1 < nrows
+            && south_conns.contains(&cur_tile)
+            && north_conns.contains(&tiles[i + 1][j])
         {
             deque.push_back((i + 1, j, distance + 1));
         }
 
-        if j > 0 && west_conns.contains(&cur_tile) && east_conns.contains(&grid[i][j - 1]) {
+        if j > 0 && west_conns.contains(&cur_tile) && east_conns.contains(&tiles[i][j - 1]) {
             deque.push_back((i, j - 1, distance + 1));
         }
 
-        if j + 1 < ncols && east_conns.contains(&cur_tile) && west_conns.contains(&grid[i][j + 1]) {
+        if j + 1 < ncols && east_conns.contains(&cur_tile) && west_conns.contains(&tiles[i][j + 1])
+        {
             deque.push_back((i, j + 1, distance + 1));
         }
     }
@@ -70,7 +73,7 @@ fn find_loop_tiles(grid: &[Vec<char>]) -> (HashSet<(usize, usize)>, usize) {
 }
 
 fn count_enclosed_tiles(
-    grid: &[Vec<char>],
+    tiles: &[Vec<char>],
     loop_tiles: &HashSet<(usize, usize)>,
     i: usize,
 ) -> usize {
@@ -79,7 +82,7 @@ fn count_enclosed_tiles(
 
     // raycast the entire row and keep track of loop
     // entrances and exits, counting enclosed tiles
-    for (j, &tile) in grid[i].iter().enumerate() {
+    for (j, &tile) in tiles[i].iter().enumerate() {
         if loop_tiles.contains(&(i, j)) {
             // we traverse the row horizontally so when
             // we cross a tile that represents a vertical
