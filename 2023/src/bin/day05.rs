fcea3c5faf2dad59711573d78c14cf751f97b9f1
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-09-11 12:14:50 +0100

    feat: day5 part 2, working reverse logic from dst to src

--- 2023/src/bin/day05.rs
+++ 2023/src/bin/day05.rs
@@ -6,11 +6,48 @@ struct MapRule {
 
 fn main() {
     let input = std::fs::read_to_string("input/day05").unwrap();
+    let (seeds, maps) = parse_input(&input);
 
+    let seeds1 = seeds.iter().map(|s| (*s, *s)).collect();
+    let p1 = find_lowest_location(&seeds1, &maps);
+    println!("Part1: {p1}");
+
+    let seeds2 = seeds.chunks(2).map(|s| (s[0], s[0] + s[1] - 1)).collect();
+    let p2 = find_lowest_location(&seeds2, &maps);
+    println!("Part2: {p2}");
+}
+
+fn find_lowest_location(seeds: &Vec<(usize, usize)>, maps: &Vec<Vec<MapRule>>) -> usize {
+    let mut location = 0;
+    loop {
+        let seed = seed_from_location(location, &maps);
+        let inside = seeds.iter().any(|(s, e)| (*s..=*e).contains(&seed));
+        if inside {
+            return location;
+        }
+        location += 1;
+    }
+}
+
+fn seed_from_location(location: usize, maps: &Vec<Vec<MapRule>>) -> usize {
+    maps.iter()
+        .rev()
+        .fold(location, |dst, rules| src_from_dst(dst, rules))
+}
+
+fn src_from_dst(dst: usize, rules: &Vec<MapRule>) -> usize {
+    rules
+        .iter()
+        .find(|r| dst >= r.dst && dst <= r.dst + r.len)
+        .map(|r| r.src + dst - r.dst)
+        .unwrap_or(dst)
+}
+
+fn parse_input(input: &str) -> (Vec<usize>, Vec<Vec<MapRule>>) {
     let mut blocks = input.split("\n\n");
 
     let (_, seeds) = blocks.next().unwrap().split_once(':').unwrap();
-    let seeds: Vec<usize> = seeds.split(' ').flat_map(str::parse).collect();
+    let seeds: Vec<usize> = seeds.split_whitespace().flat_map(str::parse).collect();
 
     let mut maps: Vec<Vec<MapRule>> = vec![];
     for block in blocks {
@@ -29,23 +66,5 @@ fn main() {
         maps.push(rules);
     }
 
-    let p1 = seeds
-        .iter()
-        .map(|seed| find_location(*seed, &maps))
-        .min()
-        .unwrap();
-    println!("Part1: {p1}");
-}
-
-fn find_location(seed: usize, maps: &[Vec<MapRule>]) -> usize {
-    maps.iter().fold(seed, |value, rule| map_value(value, rule))
-}
-
-fn map_value(value: usize, rules: &[MapRule]) -> usize {
-    let matches: Vec<usize> = rules
-        .iter()
-        .filter(|r| value >= r.src && value <= r.src + r.len)
-        .map(|r| r.dst + value - r.src)
-        .collect();
-    matches.into_iter().next().unwrap_or(value)
+    (seeds, maps)
 }
