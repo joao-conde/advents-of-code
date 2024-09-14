cb443d6ba3e16a5d1c8c0a7771a860a818692bb7
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-09-14 23:14:01 +0100

    feat: day 14 2023

--- 2023/src/bin/day14.rs
+++ 2023/src/bin/day14.rs
@@ -1,23 +1,22 @@
-use std::{collections::HashSet, str::FromStr, vec};
+use std::str::FromStr;
 
-#[derive(Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq)]
 struct Platform {
-    rocks: Vec<Vec<char>>,
-    cubes: Vec<(usize, usize)>,
+    rocks: Vec<Vec<Tile>>,
+    cubes: Vec<Cube>,
     nrows: usize,
     ncols: usize,
 }
 
 impl Platform {
-    fn load(&self) -> usize {
-        self.rocks
-            .iter()
-            .skip(1)
-            .rev()
-            .skip(1)
-            .rev()
+    fn total_load(&self) -> usize {
+        let unpadded_rocks = self.rocks.iter().skip(1).rev().skip(1).rev();
+        let unpadded_nrows = self.nrows - 2;
+        unpadded_rocks
             .enumerate()
-            .map(|(i, row)| row.iter().filter(|c| **c == 'O').count() * (self.nrows - 2 - i))
+            .map(|(i, row)| {
+                row.iter().filter(|c| **c == Tile::Round).count() * (unpadded_nrows - i)
+            })
             .sum()
     }
 
@@ -31,52 +30,52 @@ impl Platform {
     fn slide_north(&mut self) {
         let previous = self.rocks.clone();
 
-        for (ci, cj) in &self.cubes {
-            let rocks_below = (ci + 1..self.nrows)
-                .take_while(|i| previous[*i][*cj] != '#')
-                .filter(|i| previous[*i][*cj] == 'O')
+        for cube in &self.cubes {
+            let rocks_below = (cube.i + 1..self.nrows)
+                .take_while(|i| previous[*i][cube.j] != Tile::Cube)
+                .filter(|i| previous[*i][cube.j] == Tile::Round)
                 .count();
 
-            (ci + 1..=ci + rocks_below).for_each(|i| self.rocks[i][*cj] = 'O');
-            (ci + rocks_below + 1..self.nrows)
-                .take_while(|i| previous[*i][*cj] != '#')
-                .for_each(|i| self.rocks[i][*cj] = '.');
+            (cube.i + 1..=cube.i + rocks_below).for_each(|i| self.rocks[i][cube.j] = Tile::Round);
+            (cube.i + rocks_below + 1..self.nrows)
+                .take_while(|i| previous[*i][cube.j] != Tile::Cube)
+                .for_each(|i| self.rocks[i][cube.j] = Tile::Empty);
         }
     }
 
     fn slide_west(&mut self) {
         let previous = self.rocks.clone();
 
-        for (ci, cj) in &self.cubes {
-            let rocks_right = (cj + 1..self.ncols)
-                .take_while(|j| previous[*ci][*j] != '#')
-                .filter(|j| previous[*ci][*j] == 'O')
+        for cube in &self.cubes {
+            let rocks_right = (cube.j + 1..self.ncols)
+                .take_while(|j| previous[cube.i][*j] != Tile::Cube)
+                .filter(|j| previous[cube.i][*j] == Tile::Round)
                 .count();
 
-            (cj + 1..=cj + rocks_right).for_each(|j| self.rocks[*ci][j] = 'O');
-            (cj + rocks_right + 1..self.ncols)
-                .take_while(|j| previous[*ci][*j] != '#')
-                .for_each(|j| self.rocks[*ci][j] = '.');
+            (cube.j + 1..=cube.j + rocks_right).for_each(|j| self.rocks[cube.i][j] = Tile::Round);
+            (cube.j + rocks_right + 1..self.ncols)
+                .take_while(|j| previous[cube.i][*j] != Tile::Cube)
+                .for_each(|j| self.rocks[cube.i][j] = Tile::Empty);
         }
     }
 
     fn slide_south(&mut self) {
         let previous = self.rocks.clone();
 
-        for (ci, cj) in &self.cubes {
-            let to = ci.saturating_sub(1);
+        for cube in &self.cubes {
+            let to = cube.i.saturating_sub(1);
             let rocks_above = (0..=to)
                 .rev()
-                .take_while(|i| previous[*i][*cj] != '#')
-                .filter(|i| previous[*i][*cj] == 'O')
+                .take_while(|i| previous[*i][cube.j] != Tile::Cube)
+                .filter(|i| previous[*i][cube.j] == Tile::Round)
                 .count();
 
             if rocks_above > 0 {
-                (ci - rocks_above..*ci).for_each(|i| self.rocks[i][*cj] = 'O');
-                (0..ci - rocks_above)
+                (cube.i - rocks_above..cube.i).for_each(|i| self.rocks[i][cube.j] = Tile::Round);
+                (0..cube.i - rocks_above)
                     .rev()
-                    .take_while(|i| previous[*i][*cj] != '#')
-                    .for_each(|i| self.rocks[i][*cj] = '.');
+                    .take_while(|i| previous[*i][cube.j] != Tile::Cube)
+                    .for_each(|i| self.rocks[i][cube.j] = Tile::Empty);
             }
         }
     }
@@ -84,21 +83,21 @@ impl Platform {
     fn slide_east(&mut self) {
         let previous = self.rocks.clone();
 
-        for (ci, cj) in &self.cubes {
-            let to = cj.saturating_sub(1);
+        for cube in &self.cubes {
+            let to = cube.j.saturating_sub(1);
 
             let rocks_left = (0..=to)
                 .rev()
-                .take_while(|j| previous[*ci][*j] != '#')
-                .filter(|j| previous[*ci][*j] == 'O')
+                .take_while(|j| previous[cube.i][*j] != Tile::Cube)
+                .filter(|j| previous[cube.i][*j] == Tile::Round)
                 .count();
 
             if rocks_left > 0 {
-                (cj - rocks_left..*cj).for_each(|j| self.rocks[*ci][j] = 'O');
-                (0..cj - rocks_left)
+                (cube.j - rocks_left..cube.j).for_each(|j| self.rocks[cube.i][j] = Tile::Round);
+                (0..cube.j - rocks_left)
                     .rev()
-                    .take_while(|j| previous[*ci][*j] != '#')
-                    .for_each(|j| self.rocks[*ci][j] = '.');
+                    .take_while(|j| previous[cube.i][*j] != Tile::Cube)
+                    .for_each(|j| self.rocks[cube.i][j] = Tile::Empty);
             }
         }
     }
@@ -108,17 +107,20 @@ impl FromStr for Platform {
     type Err = std::io::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut rocks: Vec<Vec<char>> = s.lines().map(|l| l.chars().collect()).collect();
+        let mut rocks: Vec<Vec<Tile>> = s
+            .lines()
+            .map(|l| l.chars().map(Tile::from).collect())
+            .collect();
 
         let nrows = rocks.len();
         let ncols = rocks[0].len();
 
         // add a rock padding to the platform that
         // prevents the rocks from "falling"
-        rocks.insert(0, vec!['#'; ncols]);
-        rocks.insert(nrows + 1, vec!['#'; ncols + 2]);
-        (0..nrows + 1).for_each(|i| rocks[i].insert(0, '#'));
-        (0..nrows + 1).for_each(|i| rocks[i].insert(ncols + 1, '#'));
+        rocks.insert(0, vec![Tile::Cube; ncols]);
+        rocks.insert(nrows + 1, vec![Tile::Cube; ncols + 2]);
+        (0..nrows + 1).for_each(|i| rocks[i].insert(0, Tile::Cube));
+        (0..nrows + 1).for_each(|i| rocks[i].insert(ncols + 1, Tile::Cube));
 
         let nrows = rocks.len();
         let ncols = rocks[0].len();
@@ -126,8 +128,8 @@ impl FromStr for Platform {
         let cubes = rocks
             .iter()
             .enumerate()
-            .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, _)| (i, j)))
-            .filter(|(i, j)| rocks[*i][*j] == '#')
+            .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, _)| Cube { i, j }))
+            .filter(|cube| rocks[cube.i][cube.j] == Tile::Cube)
             .collect();
 
         Ok(Self {
@@ -139,29 +141,75 @@ impl FromStr for Platform {
     }
 }
 
+#[derive(Clone, PartialEq)]
+struct Cube {
+    i: usize,
+    j: usize,
+}
+
+#[derive(Clone, PartialEq)]
+enum Tile {
+    Round,
+    Cube,
+    Empty,
+}
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '#' => Self::Cube,
+            'O' => Self::Round,
+            '.' => Self::Empty,
+            _ => unreachable!(),
+        }
+    }
+}
+
 fn main() {
     let input = std::fs::read_to_string("input/day14").unwrap();
 
-    let mut platform = Platform::from_str(&input).unwrap();
+    let p1 = p1(&input);
+    println!("Part1: {p1}");
 
-    let mut seen = HashSet::new();
+    let p2 = p2(&input);
+    println!("Part2: {p2}");
+}
 
-    let mut states = vec![];
+fn p1(input: &str) -> usize {
+    let mut platform = Platform::from_str(input).unwrap();
+    platform.slide_north();
+    platform.total_load()
+}
 
-    for _ in 0..1_000_000_000 {
-        let state = platform.clone();
-        if seen.contains(&state) {
-            break;
-        }
-        seen.insert(state.clone());
-        states.push(state);
+fn p2(input: &str) -> usize {
+    let mut platform = Platform::from_str(input).unwrap();
 
+    // figure out when a state repeats after a cycle
+    // which will lead to the same values from here
+    // onwards i.e. there is a pattern
+    let mut states = Vec::new();
+    while !states.contains(&platform) {
+        states.push(platform.clone());
         platform.spin();
     }
 
-    let first_repeat = states.iter().position(|s| *s == platform).unwrap();
+    // the index where the pattern first repeats
+    // everything before this was not part of
+    // the pattern yet
+    let pattern_start = states.iter().position(|s| *s == platform).unwrap();
+    let pattern_length = states.len() - pattern_start;
 
-    let state_i = (1_000_000_000 - first_repeat) % (states.len() - first_repeat) + first_repeat;
+    // the number of iterations where the pattern
+    // actually repeats
+    let pattern_iterations = 1_000_000_000 - pattern_start;
 
-    println!("{}", states[state_i].load());
+    // figure out where in our pattern the last iteration ends
+    let last_iteration_pattern_index = pattern_iterations % pattern_length;
+
+    // adding the offset for the pattern to start with the final
+    // pattern index gives us the state index to look for
+    let last_state_index = pattern_start + last_iteration_pattern_index;
+    let last_state = &states[last_state_index];
+
+    last_state.total_load()
 }
