232fa8caafd8fa5a92c8639abd0e4d4a824300cd
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-09-14 03:15:27 +0100

    feat: aoc 2023 day17 p1

--- 2023/src/bin/day17.rs
+++ 2023/src/bin/day17.rs
@@ -1,4 +1,122 @@
+use std::cmp::Ordering;
 use std::collections::{BinaryHeap, HashSet};
+use std::hash::Hash;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn turn_right(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+        }
+    }
+
+    fn turn_left(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Down => Direction::Right,
+            Direction::Left => Direction::Down,
+            Direction::Right => Direction::Up,
+        }
+    }
+
+    fn delta(&self) -> (isize, isize) {
+        match self {
+            Direction::Up => (-1, 0),
+            Direction::Down => (1, 0),
+            Direction::Left => (0, -1),
+            Direction::Right => (0, 1),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq)]
+struct State {
+    i: usize,
+    j: usize,
+    direction: Direction,
+    streak: usize,
+    cost: u32,
+}
+
+impl Hash for State {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.i.hash(state);
+        self.j.hash(state);
+        self.direction.hash(state);
+        self.streak.hash(state);
+    }
+}
+
+impl PartialEq for State {
+    fn eq(&self, other: &Self) -> bool {
+        self.i == other.i
+            && self.j == other.j
+            && self.direction == other.direction
+            && self.streak == other.streak
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost.cmp(&other.cost).reverse()
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl State {
+    fn forward(&self, map: &Vec<Vec<u32>>) -> Option<State> {
+        let nrows = map.len();
+        let ncols = map[0].len();
+
+        let (di, dj) = self.direction.delta();
+
+        let ni = self.i.checked_add_signed(di).filter(|i| *i < nrows)?;
+        let nj = self.j.checked_add_signed(dj).filter(|j| *j < ncols)?;
+
+        Some(Self {
+            i: ni,
+            j: nj,
+            direction: self.direction,
+            streak: self.streak + 1,
+            cost: self.cost + map[ni][nj],
+        })
+    }
+
+    fn turn_right(&self) -> State {
+        Self {
+            i: self.i,
+            j: self.j,
+            direction: self.direction.turn_right(),
+            streak: 0,
+            cost: self.cost,
+        }
+    }
+
+    fn turn_left(&self) -> State {
+        Self {
+            i: self.i,
+            j: self.j,
+            direction: self.direction.turn_left(),
+            streak: 0,
+            cost: self.cost,
+        }
+    }
+}
 
 fn main() {
     let input = std::fs::read_to_string("input/day17").unwrap();
@@ -11,38 +129,46 @@ fn main() {
     let nrows = map.len();
     let ncols = map[0].len();
 
-    let mut points: BinaryHeap<(u32, usize, usize, usize, char, String)> = BinaryHeap::new();
+    let mut points: BinaryHeap<State> = BinaryHeap::new();
     let mut visited = HashSet::new();
 
-    points.push((0, 0, 0, 1, 'r', "r".to_owned()));
-    points.push((0, 0, 0, 1, 'd', "d".to_owned()));
-    while let Some((cost, i, j, distance, dir, dirs)) = points.pop() {
-        if i == nrows - 1 && j == ncols - 1 {
-            println!("found it with {cost} {dirs}");
+    points.push(State {
+        i: 0,
+        j: 0,
+        direction: Direction::Right,
+        streak: 0,
+        cost: 0,
+    });
+
+    while let Some(state) = points.pop() {
+        if state.i == nrows - 1 && state.j == ncols - 1 {
+            dbg!(&state);
+            println!("found it with {}", state.cost);
             return;
         }
 
-        if visited.contains(&(i, j, cost, distance)) {
+        if state.i >= nrows || state.j >= ncols {
             continue;
         }
 
-        visited.insert((i, j, cost, distance));
+        if visited.contains(&(state.i, state.j, state.direction, state.streak)) {
+            continue;
+        }
+        visited.insert((state.i, state.j, state.direction, state.streak));
 
-        let cost = cost + map[i][j];
-
-        if i < nrows - 1 && (dir != 'd' || distance != 3) {
-            let distance = if dir != 'd' { 1 } else { distance + 1 };
-            points.push((cost, i + 1, j, distance, 'd', format!("{dirs}d")));
+        if let Some(state) = state.turn_right().forward(&map) {
+            points.push(state);
         }
 
-        if j < ncols - 1 && (dir != 'r' || distance != 3) {
-            let distance = if dir != 'r' { 1 } else { distance + 1 };
-            points.push((cost, i, j + 1, distance, 'r', format!("{dirs}r")));
+        if let Some(state) = state.turn_left().forward(&map) {
+            points.push(state);
         }
 
-        if i > 0 && (dir != 'u' || distance != 3) {
-            let distance = if dir != 'u' { 1 } else { distance + 1 };
-            points.push((cost, i - 1, j, distance, 'u', format!("{dirs}u")));
+        // if we dont have to move yet, explore going forward
+        if state.streak < 3 {
+            if let Some(state) = state.forward(&map) {
+                points.push(state);
+            }
         }
     }
 }
