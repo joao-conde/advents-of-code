19ff5d6f19a086c26f0f550ef3b7cd164e41b106
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-09-14 13:10:54 +0100

    feat: 2023 day9 p1

---
+++ 2023/src/bin/day09.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let input = std::fs::read_to_string("input/day09").unwrap();
+
+    let report: Vec<Vec<isize>> = input
+        .lines()
+        .map(|l| l.split_whitespace().map(|x| x.parse().unwrap()).collect())
+        .collect();
+
+    let p1: isize = report.iter().map(expand).map(extrapolate).sum();
+
+    dbg!(p1);
+}
+
+fn expand(history: &Vec<isize>) -> Vec<Vec<isize>> {
+    let mut sequence = history.clone();
+    let mut sequences = vec![sequence.clone()];
+
+    while !sequence.iter().all(|x| *x == 0) {
+        let new_sequence: Vec<isize> = sequence.windows(2).map(|w| w[1] - w[0]).collect();
+        sequences.push(new_sequence.clone());
+        sequence = new_sequence;
+    }
+
+    sequences
+}
+
+fn extrapolate(predictions: Vec<Vec<isize>>) -> isize {
+    predictions
+        .iter()
+        .rev()
+        .fold(0, |last, seq| seq.last().unwrap() + last)
+}
