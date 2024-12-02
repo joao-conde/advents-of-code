05cfc25ce0f808c4b7d7c8f78b6cafedf2ed474e
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-02 00:20:25 +0000

    chore: change module name

--- 2024/mix.exs
+++ 2024/mix.exs
@@ -1,9 +1,9 @@
-defmodule Advent.MixProject do
+defmodule MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :aoc_2024,
+      app: :aoc2024,
       version: "0.1.0",
       elixir: "~> 1.17.3",
       deps: deps(),
