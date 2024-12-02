e470e09fe35e83b864df79f2730b8b0ac14efb4e
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-02 00:19:23 +0000

    feat: add format and lint jobs to elixir project

--- 2024/mix.exs
+++ 2024/mix.exs
@@ -5,7 +5,25 @@ defmodule Advent.MixProject do
     [
       app: :aoc_2024,
       version: "0.1.0",
-      elixir: "~> 1.17.3"
+      elixir: "~> 1.17.3",
+      deps: deps(),
+      aliases: aliases()
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.7.10", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: "credo",
+      precommit: [
+        "format",
+        "lint"
+      ]
     ]
   end
 end
