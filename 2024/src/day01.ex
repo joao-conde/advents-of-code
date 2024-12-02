f7bd288bfd1952aafad4c564d810c34b3fe1d4e2
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-02 20:57:17 +0000

    refactor: day1 module

--- 2024/src/day01.ex
+++ 2024/src/day01.ex
@@ -1,20 +1,31 @@
-[left, right] =
-  File.read!("input/day01")
-  |> String.split("\n")
-  |> Enum.map(&String.split/1)
-  |> Enum.map(fn pair -> Enum.map(pair, &String.to_integer/1) end)
-  |> Enum.reduce([[], []], fn [x1, x2], [left, right] -> [[x1 | left], [x2 | right]] end)
-  |> Enum.map(fn list -> Enum.sort(list) end)
+defmodule Day01 do
+  def solve do
+    [left, right] = parse_lists("input/day01")
+    IO.inspect(part1(left, right), label: "Part1")
+    IO.inspect(part2(left, right), label: "Part2")
+  end
 
-# part one
-Enum.zip(left, right)
-|> Enum.map(fn {x1, x2} -> abs(x2 - x1) end)
-|> Enum.sum()
-|> IO.inspect(label: "Part1")
+  def part1(left, right) do
+    Enum.zip(left, right)
+    |> Enum.map(fn {x1, x2} -> abs(x2 - x1) end)
+    |> Enum.sum()
+  end
 
-# part two
-frequencies = Enum.frequencies(right)
+  def part2(left, right) do
+    frequencies = Enum.frequencies(right)
 
-Enum.map(left, fn x1 -> x1 * Map.get(frequencies, x1, 0) end)
-|> Enum.sum()
-|> IO.inspect(label: "Part2")
+    Enum.map(left, fn x1 -> x1 * Map.get(frequencies, x1, 0) end)
+    |> Enum.sum()
+  end
+
+  def parse_lists(input) do
+    File.read!(input)
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn pair -> Enum.map(pair, &String.to_integer/1) end)
+    |> Enum.reduce([[], []], fn [x1, x2], [left, right] -> [[x1 | left], [x2 | right]] end)
+    |> Enum.map(fn list -> Enum.sort(list) end)
+  end
+end
+
+Day01.solve()
