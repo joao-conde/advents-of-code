e22eb0b7f9ee1bb74482f9e7fd0b9434dd6b8cef
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-03 12:42:54 +0000

    feat: day 2 2024

---
+++ 2024/src/day02.ex
@@ -0,0 +1,71 @@
+defmodule Day02 do
+  def solve do
+    reports = parse_reports("input/day02")
+
+    p1 = safe_reports(reports, 0)
+    IO.puts("Part1: #{p1}")
+
+    p2 = safe_reports(reports, 1)
+    IO.puts("Part2: #{p2}")
+  end
+
+  def safe_reports(reports, allowed) do
+    Enum.count(reports, fn report -> safe_report(report, allowed) end)
+  end
+
+  def safe_report(report, allowed) do
+    mistake_index = find_report_mistake(report)
+
+    cond do
+      allowed < 0 ->
+        false
+
+      mistake_index == nil ->
+        true
+
+      true ->
+        removed_left = remove_index(report, mistake_index)
+        removed_right = remove_index(report, mistake_index + 1)
+        safe_report(removed_left, allowed - 1) || safe_report(removed_right, allowed - 1)
+    end
+  end
+
+  def find_report_mistake(report) do
+    diffs =
+      report
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [x1, x2] -> x1 - x2 end)
+
+    positives = Enum.count(diffs, fn x -> x > 0 end)
+    negatives = length(diffs) - positives
+
+    sorted_by =
+      if positives > negatives do
+        :decreasing
+      else
+        :increasing
+      end
+
+    Enum.find_index(diffs, fn diff -> !valid_diff(diff, sorted_by) end)
+  end
+
+  def valid_diff(diff, sorted_by) do
+    abs(diff) >= 1 && abs(diff) <= 3 &&
+      ((sorted_by == :decreasing && diff > 0) ||
+         (sorted_by == :increasing && diff < 0))
+  end
+
+  def parse_reports(input) do
+    File.read!(input)
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.map(fn report -> Enum.map(report, &String.to_integer/1) end)
+  end
+
+  def remove_index(list, index) do
+    Enum.slice(list, 0, index) ++
+      Enum.slice(list, index + 1, length(list) - index)
+  end
+end
+
+Day02.solve()
