ba9a84b16f5edb4edfe376c35a3e1865fda11622
Joao Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-07 20:10:19 +0000

    feat: day7 p2

--- 2024/src/day07.ex
+++ 2024/src/day07.ex
@@ -2,16 +2,16 @@ defmodule Day07 do
   def solve do
     equations = parse_equations("input/day07")
 
-    p1 = total_calibration(equations)
+    p1 = total_calibration(equations, &solutions/1)
     IO.puts("Part1: #{p1}")
 
-    p2 = 0
+    p2 = total_calibration(equations, &solutions2/1)
     IO.puts("Part2: #{p2}")
   end
 
-  def total_calibration(equations) do
+  def total_calibration(equations, solve) do
     equations
-    |> Enum.filter(fn {result, operands} -> result in solutions(operands) end)
+    |> Enum.filter(fn {result, operands} -> result in solve.(operands) end)
     |> Enum.map(fn {result, _} -> result end)
     |> Enum.sum()
   end
@@ -21,6 +21,13 @@ defmodule Day07 do
   def solutions([operand | operands]) do
     solutions(operands)
     |> Enum.flat_map(fn s -> [operand * s, operand + s] end)
+  end
+
+  def solutions2([operand]), do: [operand]
+
+  def solutions2([operand | operands]) do
+    solutions2(operands)
+    |> Enum.flat_map(fn s -> [operand * s, operand + s, String.to_integer("#{s}#{operand}")] end)
   end
 
   def parse_equations(input) do
