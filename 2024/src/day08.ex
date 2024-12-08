c922575de95bbf95fb82fb79ff5def7d13134094
João Conde <16060539+joao-conde@users.noreply.github.com>
2024-12-08 17:37:24 +0000

    refactor: day8 2024

--- 2024/src/day08.ex
+++ 2024/src/day08.ex
@@ -2,45 +2,35 @@ defmodule Day08 do
   def solve do
     {antennas, width, height} = parse_antennas("input/day08")
 
-    p1 =
-      antennas
-      |> Map.values()
-      |> Enum.flat_map(fn a ->
-        a
-        |> pairings()
-        |> Enum.flat_map(fn {{i1, j1}, {i2, j2}} ->
-          {vi, vj} = vector({i1, j1}, {i2, j2})
-          [{i1 - vi, j1 - vj}, {i2 + vi, j2 + vj}]
-        end)
-      end)
-      |> Enum.filter(fn location ->
-        not out_of_bounds(location, width, height)
-      end)
-      |> MapSet.new()
-      |> MapSet.size()
-
+    p1 = antinode_locations(antennas, width, height, 1..1)
     IO.puts("Part1: #{p1}")
 
-    p2 =
-      antennas
-      |> Map.values()
-      |> Enum.flat_map(fn a ->
-        a
-        |> pairings()
-        |> Enum.flat_map(fn {{i1, j1}, {i2, j2}} ->
-          {vi, vj} = vector({i1, j1}, {i2, j2})
-
-          0..100
-          |> Enum.flat_map(fn i -> [{i1 - vi * i, j1 - vj * i}, {i2 + vi * i, j2 + vj * i}] end)
-        end)
-      end)
-      |> Enum.filter(fn location ->
-        not out_of_bounds(location, width, height)
-      end)
-      |> MapSet.new()
-      |> MapSet.size()
-
+    max_repeat = max(width, height)
+    p2 = antinode_locations(antennas, width, height, 0..max_repeat)
     IO.puts("Part2: #{p2}")
+  end
+
+  def antinode_locations(antennas, width, height, repeat_range) do
+    antennas
+    |> Map.values()
+    |> Enum.flat_map(fn a ->
+      a
+      |> pairings()
+      |> Enum.flat_map(fn {l1, l2} -> antinodes(l1, l2, repeat_range) end)
+    end)
+    |> Enum.filter(fn location ->
+      not out_of_bounds(location, width, height)
+    end)
+    |> MapSet.new()
+    |> MapSet.size()
+  end
+
+  def antinodes({i1, j1}, {i2, j2}, repeat_range) do
+    {vi, vj} = vector({i1, j1}, {i2, j2})
+
+    Enum.flat_map(repeat_range, fn i ->
+      [{i1 - vi * i, j1 - vj * i}, {i2 + vi * i, j2 + vj * i}]
+    end)
   end
 
   def pairings(antennas) do
